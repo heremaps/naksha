65851f63f757435315984db39205980ea388a5be
Hiren Patel <hiren.patel@here.com>
2023-05-30 21:33:20 +0530

    Cleaning up before merging Naksha_master

--- xyz-hub-service/src/main/java/com/here/xyz/hub/rest/admin/messages/brokers/IGetBroker.java
+++
@@ -1,9 +0,0 @@
-package com.here.xyz.hub.rest.admin.messages.brokers;
-
-import com.here.xyz.hub.rest.admin.MessageBroker;
-import org.jetbrains.annotations.NotNull;
-
-@FunctionalInterface
-public interface IGetBroker {
-  @NotNull MessageBroker get();
-}
