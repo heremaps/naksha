8b0a89a0cbd9c8bdff08a25a1daf6f8190fcc045
phmai <phuc.mai@here.com>
2023-11-08 14:31:23 +0100

    add check for handler creation request without className or storageId, with test

--- here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/IntHandlerForEventHandlers.java
+++ here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/IntHandlerForEventHandlers.java
@@ -21,6 +21,9 @@ package com.here.naksha.lib.handlers;
 import com.here.naksha.lib.core.IEvent;
 import com.here.naksha.lib.core.INaksha;
 import com.here.naksha.lib.core.NakshaContext;
+import com.here.naksha.lib.core.models.XyzError;
+import com.here.naksha.lib.core.models.naksha.EventHandler;
+import com.here.naksha.lib.core.models.naksha.EventHandlerProperties;
 import com.here.naksha.lib.core.models.storage.*;
 import com.here.naksha.lib.core.storage.IReadSession;
 import com.here.naksha.lib.core.storage.IWriteSession;
@@ -48,7 +51,10 @@ public class IntHandlerForEventHandlers extends AbstractEventHandler {
         return reader.execute(rr);
       }
     } else if (request instanceof WriteRequest<?, ?> wr) {
-      // TODO check if storageId is provided
+      // validate the request before persisting
+      final Result valResult = validateWriteRequest(wr);
+      if (valResult instanceof ErrorResult er) return er;
+      // persist in storage
       try (final IWriteSession writer = nakshaHub().getAdminStorage().newWriteSession(ctx, true)) {
         final Result result = writer.execute(wr);
         if (result instanceof SuccessResult) writer.commit();
@@ -57,5 +63,23 @@ public class IntHandlerForEventHandlers extends AbstractEventHandler {
     } else {
       return notImplemented(event);
     }
+  }
+
+  private @NotNull Result validateWriteRequest(final @NotNull WriteRequest<?, ?> wr) {
+    Result result = null;
+    for (final WriteOp<?> wOp : wr.queries) {
+      final EventHandler eventHandler = (EventHandler) wOp.feature;
+      // Common Plugin specific validations
+      result = validateWritePluginRequest(eventHandler);
+      if (result instanceof ErrorResult) return result;
+      // TODO : handler specific validations in future, as needed
+      // TODO check if storageId is valid
+      final Object storageId = eventHandler.getProperties().get(EventHandlerProperties.STORAGE_ID);
+      if (storageId == null)
+        return new ErrorResult(XyzError.ILLEGAL_ARGUMENT, "Mandatory parameter properties.storageId missing!");
+      if (storageId.toString().isEmpty())
+        return new ErrorResult(XyzError.ILLEGAL_ARGUMENT, "Mandatory parameter properties.storageId missing!");
+    }
+    return new SuccessResult();
   }
 }
