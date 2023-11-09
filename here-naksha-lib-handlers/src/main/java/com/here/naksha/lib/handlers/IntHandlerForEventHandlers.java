d790c20e6953acbfa68a4b88c2bf3bb69c0f546a
phmai <phuc.mai@here.com>
2023-11-09 09:46:13 +0100

    add jira ticket to todo, modify static class name

--- here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/IntHandlerForEventHandlers.java
+++ here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/IntHandlerForEventHandlers.java
@@ -75,11 +75,11 @@ public class IntHandlerForEventHandlers extends AbstractEventHandler {
       result = validateWritePluginRequest(eventHandler);
       if (result instanceof ErrorResult) return result;
       // TODO : handler specific validations in future, as needed
-      if (eventHandler.getClassName().equals("com.here.naksha.lib.handlers.DefaultStorageHandler")) {
+      if (eventHandler.getClassName().equals(DefaultStorageHandler.class.getName())) {
         final Object storageId = eventHandler.getProperties().get(EventHandlerProperties.STORAGE_ID);
         if (storageId == null || storageId.toString().isEmpty())
           return new ErrorResult(XyzError.ILLEGAL_ARGUMENT, "Mandatory parameter properties.storageId missing!");
-        // TODO check if storageId is valid
+        // TODO MCPODS-6574 check if storageId is valid
       }
     }
     return new SuccessResult();
