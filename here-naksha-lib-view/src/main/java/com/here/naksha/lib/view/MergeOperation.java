ed23789e732e3e8ba6e15c38b35581faae70fb31
Pawel Mazurek <mazurek@J49L39Q5YW.ad.here.com>
2023-12-20 11:55:01 +0100

    MCPODS-6434 add comments

--- here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/MergeOperation.java
+++ here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/MergeOperation.java
@@ -23,6 +23,20 @@ import java.util.List;
 
 public interface MergeOperation<FEATURE, CODEC extends FeatureCodec<FEATURE, CODEC>> {
 
+  /**
+   * This operation is used to combine results from multiple layers into one.
+   * The input is one single feature (correlated by ID) returned by multiple layers:
+   * {@code
+   * [Layer_0:Feature1, Layer_1:Feature1, Layer_2:Feature1, ...]
+   * }
+   * What's important - the input have only layers where feature was found. <br>
+   *
+   * It's up to implementation how feature will be reduced to 1. It might be a feature from one specific layer
+   * or merged feature from few.
+   *
+   * @param multipleResults
+   * @return
+   */
   // TODO should we know from which storage result comes from? If yes then we should return SingleStorageRow instead.
   CODEC apply(List<ViewLayerRow<FEATURE, CODEC>> multipleResults);
 }
--- here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/MissingIdResolver.java
+++ here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/MissingIdResolver.java
@@ -23,17 +23,20 @@ import java.util.List;
 import org.apache.commons.lang3.tuple.Pair;
 
 /**
- * Defines the strategy for missing feature in one of the layers.
+ * Defines the strategy for missing feature in one of the layers. <br>
  *
  * Consider this example:
  * Result from Storage A: [F_1, F_2, F_3, F_4]
  * Result from Storage B: [F_2, F_4]
- * Result from Storage C: [F_3, F_5]
+ * Result from Storage C: [F_3, F_5] <br>
+ *
+ * The feature F_1 is missing in results from storage B and C, by providing {@link MissingIdResolver} you can decide
+ * what to do with it, you can decide to try to fetch F_1 by id or to ignore it. <br>
  *
  * Here are couple examples of possible implementations:
  * 1. Ignore fetching more - perfect when your basic query is by ID (it doesn't make sense to search it again).
  * 2. Calculate missing features' IDs and try to fetch them by ID - good when your basic query is i.e. by Bbox
- * 3. Fetch only if feature is missing in specific Storage and from specific Storage.
+ * 3. Fetch only if feature is missing in specific Storage and query again only that Storage/layer.
  */
 public interface MissingIdResolver<FEATURE, CODEC extends FeatureCodec<FEATURE, CODEC>> {
 
@@ -46,8 +49,8 @@ public interface MissingIdResolver<FEATURE, CODEC extends FeatureCodec<FEATURE, 
 
   /**
    * Returns Pair<Layer,FEATURE_ID> that will be fetched in second query.
-   * As an input you get feature in all layers, you can decide whether you want to query again (by ID) only
-   * in specific layer or in all with missing feature. <br><br>
+   * As an input you get feature from layers where it was found, you can decide whether you want to query again (by ID) only
+   * specific layer or all where feature is missing. <br>
    *
    * You can also implement your own way of ID calculation (for fetching) - maybe your json contains information about previous id:
    * { "id": "2345", "old_id": "11223" }
