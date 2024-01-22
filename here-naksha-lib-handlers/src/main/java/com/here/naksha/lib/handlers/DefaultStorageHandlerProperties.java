03d41a3d837d53e4de2758d677f47d64fa35af56
Jakub Amanowicz <jakub.amanowicz@here.com>
2024-01-22 10:12:05 +0100

    MCPODS-6206 autoDelete and autoCreate props

--- here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/DefaultStorageHandlerProperties.java
+++ here-naksha-lib-handlers/src/main/java/com/here/naksha/lib/handlers/DefaultStorageHandlerProperties.java
@@ -26,6 +26,7 @@ import com.here.naksha.lib.core.models.naksha.SpaceProperties;
 import com.here.naksha.lib.core.models.naksha.Storage;
 import com.here.naksha.lib.core.models.naksha.XyzCollection;
 import org.jetbrains.annotations.ApiStatus.AvailableSince;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -34,11 +35,20 @@ import org.jetbrains.annotations.Nullable;
 @AvailableSince(NakshaVersion.v2_0_7)
 public class DefaultStorageHandlerProperties extends XyzProperties {
 
+  private static final Boolean DEFAULT_AUTO_CREATE_COLLECTION = true;
+  private static final Boolean DEFAULT_AUTO_DELETE_COLLECTION = true;
+
   @AvailableSince(NakshaVersion.v2_0_7)
   public static final String STORAGE_ID = "storageId";
 
   @AvailableSince(NakshaVersion.v2_0_7)
   public static final String COLLECTION = "collection";
+
+  @AvailableSince(NakshaVersion.v2_0_7)
+  public static final String AUTO_CREATE_COLLECTION = "autoCreateCollection";
+
+  @AvailableSince(NakshaVersion.v2_0_7)
+  public static final String AUTO_DELETE_COLLECTION = "autoDeleteCollection";
 
   /**
    * To associate EventHandler with specific {@link Storage} that it should operate against.
@@ -56,6 +66,22 @@ public class DefaultStorageHandlerProperties extends XyzProperties {
   private @Nullable XyzCollection xyzCollection;
 
   /**
+   * Indicates whether collection should be created automatically (happens on first collection's usage).
+   * By default: 'true'
+   */
+  @AvailableSince(NakshaVersion.v2_0_7)
+  @JsonProperty(AUTO_CREATE_COLLECTION)
+  private @NotNull Boolean autoCreateCollection;
+
+  /**
+   * Indicates whether collection should be deleted automatically (happens when handler is deleted).
+   * By default: 'true'
+   */
+  @AvailableSince(NakshaVersion.v2_0_7)
+  @JsonProperty(AUTO_CREATE_COLLECTION)
+  private @NotNull Boolean autoDeleteCollection;
+
+  /**
    * Create new EventHandler properties with storageId and collection details
    *
    * @param xyzCollection details of backend xyz collection
@@ -64,9 +90,15 @@ public class DefaultStorageHandlerProperties extends XyzProperties {
   @JsonCreator
   public DefaultStorageHandlerProperties(
       final @JsonProperty(STORAGE_ID) @Nullable String storageId,
-      final @JsonProperty(COLLECTION) @Nullable XyzCollection xyzCollection) {
+      final @JsonProperty(COLLECTION) @Nullable XyzCollection xyzCollection,
+      final @JsonProperty(AUTO_CREATE_COLLECTION) Boolean autoCreateCollection,
+      final @JsonProperty(AUTO_DELETE_COLLECTION) Boolean autoDeleteCollection) {
     this.storageId = storageId;
     this.xyzCollection = xyzCollection;
+    this.autoCreateCollection =
+        autoCreateCollection == null ? DEFAULT_AUTO_CREATE_COLLECTION : autoCreateCollection;
+    this.autoDeleteCollection =
+        autoDeleteCollection == null ? DEFAULT_AUTO_DELETE_COLLECTION : autoDeleteCollection;
   }
 
   public @Nullable XyzCollection getXyzCollection() {
@@ -83,5 +115,21 @@ public class DefaultStorageHandlerProperties extends XyzProperties {
 
   public void setStorageId(final @Nullable String storageId) {
     this.storageId = storageId;
+  }
+
+  public @NotNull Boolean getAutoCreateCollection() {
+    return autoCreateCollection;
+  }
+
+  public void setAutoCreateCollection(Boolean autoCreateCollection) {
+    this.autoCreateCollection = autoCreateCollection;
+  }
+
+  public @NotNull Boolean getAutoDeleteCollection() {
+    return autoDeleteCollection;
+  }
+
+  public void setAutoDeleteCollection(Boolean autoDeleteCollection) {
+    this.autoDeleteCollection = autoDeleteCollection;
   }
 }
