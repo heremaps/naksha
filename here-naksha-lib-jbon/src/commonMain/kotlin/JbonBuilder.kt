ed4da4ef702697d4accdc57def0db5a261919d58
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-01-24 02:02:36 +0100

    Improve tests, fix bugs, add bool, float, double, null and undefined support

--- here-naksha-lib-jbon/src/commonMain/kotlin/JbonBuilder.kt
+++ here-naksha-lib-jbon/src/commonMain/kotlin/JbonBuilder.kt
@@ -5,16 +5,25 @@ package com.here.naksha.lib.jbon;
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
 
-const val TYPE_INT4 = 0;
-const val TYPE_SIZE32 = 2 shl 4;
-const val TYPE_INT8 = 8 shl 4;
-const val TYPE_INT16 = 9 shl 4;
-const val TYPE_INT24 = 10 shl 4;
-const val TYPE_INT32 = 11 shl 4;
-const val TYPE_INT40 = 12 shl 4;
-const val TYPE_INT48 = 13 shl 4;
-const val TYPE_INT56 = 14 shl 4;
-const val TYPE_INT64 = 15 shl 4;
+const val TYPE_INT4 = 0b1000_0000
+const val TYPE_FLOAT4 = 0b1001_0000
+const val TYPE_SIZE32 = 0b1010_0000
+const val TYPE_NULL = 0
+const val TYPE_UNDEFINED = 1
+const val TYPE_BOOL_TRUE = 2
+const val TYPE_BOOL_FALSE = 3
+const val TYPE_FLOAT32 = 4
+const val TYPE_FLOAT64 = 5
+const val TYPE_INT8 = 8
+const val TYPE_INT16 = 9
+const val TYPE_INT24 = 10
+const val TYPE_INT32 = 11
+const val TYPE_INT40 = 12
+const val TYPE_INT48 = 13
+const val TYPE_INT56 = 14
+const val TYPE_INT64 = 15
+val TINY_FLOATS = floatArrayOf(-8f, -7f, -6f, -5f, -4f, -3f, -2f, -1f, 0f, 1f, 2f, 3f, 4f, 5f, 6f, 7f)
+val TINY_DOUBLES = doubleArrayOf(-8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0)
 
 @JsExport
 open class JbonBuilder(var view: IDataView, val dictionary: JbonDict? = null) {
@@ -24,17 +33,39 @@ open class JbonBuilder(var view: IDataView, val dictionary: JbonDict? = null) {
      */
     var end: Int = 0;
 
-    fun clear() : Int {
+    fun clear(): Int {
         val old = end
         end = 0
         return old
+    }
+
+    fun writeNull() : Int {
+        val pos = end;
+        view.setInt8(end++, TYPE_NULL.toByte())
+        return pos;
+    }
+
+    fun writeUndefined() : Int {
+        val pos = end;
+        view.setInt8(end++, TYPE_UNDEFINED.toByte())
+        return pos;
+    }
+
+    fun writeBool(value : Boolean) : Int {
+        val pos = end;
+        if (value) {
+            view.setInt8(end++, TYPE_BOOL_TRUE.toByte())
+        } else {
+            view.setInt8(end++, TYPE_BOOL_FALSE.toByte())
+        }
+        return pos;
     }
 
     fun writeInt(value: Int): Int {
         val pos = end;
         if (value >= -8 && value <= 7) {
             // encode int4
-            view.setInt8(pos, (value + 8).toByte())
+            view.setInt8(pos, (TYPE_INT4 xor (value + 8)).toByte())
             end += 1
         } else if (value >= -128 && value <= 127) {
             view.setInt8(pos, TYPE_INT8.toByte())
@@ -52,15 +83,46 @@ open class JbonBuilder(var view: IDataView, val dictionary: JbonDict? = null) {
         return pos
     }
 
-    fun writeSize32(value: Int) : Int {
-        val pos = end;
+    fun writeFloat(value: Float): Int {
+        val pos = end
+        for (i in 0..15) {
+            val tiny = TINY_FLOATS[i]
+            if (tiny == value) {
+                view.setInt8(end++, (TYPE_FLOAT4 xor i).toByte())
+                return pos
+            }
+        }
+        view.setInt8(end, TYPE_FLOAT32.toByte())
+        view.setFloat32(end + 1, value)
+        end += 5
+        return pos
+    }
+
+    fun writeDouble(value: Double): Int {
+        val pos = end
+        for (i in 0..15) {
+            val tiny = TINY_DOUBLES[i]
+            if (tiny == value) {
+                view.setInt8(end++, (TYPE_FLOAT4 xor i).toByte())
+                return pos
+            }
+        }
+        view.setInt8(end, TYPE_FLOAT64.toByte())
+        view.setFloat64(end + 1, value)
+        end += 9
+        return pos
+    }
+
+    fun writeSize32(value: Int): Int {
+        val pos = end
         if (value < 0) throw Exception("the value must not be negative")
         when (value) {
             in 0..11 -> view.setInt8(end++, (TYPE_SIZE32 xor value).toByte())
-            in 12 .. 267 -> {
+            in 12..267 -> {
                 view.setInt8(end++, (TYPE_SIZE32 xor 12).toByte())
-                view.setInt8(end++, ((value-12) and 0xff).toByte())
+                view.setInt8(end++, ((value - 12) and 0xff).toByte())
             }
+
             in 268..65535 -> {
                 view.setInt8(end++, (TYPE_SIZE32 xor 13).toByte())
                 view.setInt16(end, (value and 0xffff).toShort())
--- here-naksha-lib-jbon/src/jvmTest/kotlin/JvmJbonTest.kt
+++ here-naksha-lib-jbon/src/jvmTest/kotlin/JvmJbonTest.kt
@@ -1,6 +1,4 @@
-import com.here.naksha.lib.jbon.JbonBuilder
-import com.here.naksha.lib.jbon.TYPE_INT4
-import com.here.naksha.lib.jbon.TYPE_SIZE32
+import com.here.naksha.lib.jbon.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
@@ -19,9 +17,39 @@ class JvmJbonTest {
 
     @Test
     fun pureTest() {
-        val expected : Byte = -1
-        val intValue : Int = 255
+        val expected: Byte = -1
+        val intValue: Int = 255
         assertEquals(expected, (intValue and 0xff).toByte())
+    }
+
+    @Test
+    fun testNull() {
+        val view = JvmPlatform.dataViewOf(ByteArray(256))
+        val builder = JbonBuilder(view)
+        builder.writeNull()
+        assertEquals(TYPE_NULL, view.getInt8(0).toInt())
+        assertEquals(1, builder.clear())
+    }
+
+    @Test
+    fun testUndefined() {
+        val view = JvmPlatform.dataViewOf(ByteArray(256))
+        val builder = JbonBuilder(view)
+        builder.writeUndefined()
+        assertEquals(TYPE_UNDEFINED, view.getInt8(0).toInt())
+        assertEquals(1, builder.clear())
+    }
+
+    @Test
+    fun testBoolean() {
+        val view = JvmPlatform.dataViewOf(ByteArray(256))
+        val builder = JbonBuilder(view)
+        builder.writeBool(true)
+        assertEquals(TYPE_BOOL_TRUE, view.getInt8(0).toInt())
+        assertEquals(1, builder.clear())
+        builder.writeBool(false)
+        assertEquals(TYPE_BOOL_FALSE, view.getInt8(0).toInt())
+        assertEquals(1, builder.clear())
     }
 
     @Test
@@ -30,32 +58,68 @@ class JvmJbonTest {
         val builder = JbonBuilder(view)
         // the values -8 to 7 should be encoded in one byte
         builder.writeInt(-8);
-        assertEquals(builder.clear(), 1)
+        assertEquals(1, builder.clear())
         builder.writeInt(7);
-        assertEquals(builder.clear(), 1)
+        assertEquals(1, builder.clear())
         // the values below -8 and above 7 should be encoded in two byte
         builder.writeInt(-9);
         assertEquals(-9, view.getInt8(1))
-        assertEquals(builder.clear(), 2)
+        assertEquals(2, builder.clear())
         builder.writeInt(8);
         assertEquals(8, view.getInt8(1))
-        assertEquals(builder.clear(), 2)
+        assertEquals(2, builder.clear())
         // a value less than -128 must be stored in three byte
         builder.writeInt(-129)
         assertEquals(-129, view.getInt16(1))
-        assertEquals(builder.clear(), 3)
+        assertEquals(3, builder.clear())
         // a value bigger than 127 must be stored in three byte
         builder.writeInt(128)
         assertEquals(128, view.getInt16(1))
-        assertEquals(builder.clear(), 3)
+        assertEquals(3, builder.clear())
         // a value less than -32768 must be stored in five byte
         builder.writeInt(-32769)
         assertEquals(-32769, view.getInt32(1))
-        assertEquals(builder.clear(), 5)
+        assertEquals(5, builder.clear())
         // a value bigger than 32767 must be stored in three byte
         builder.writeInt(32768)
         assertEquals(32768, view.getInt32(1))
-        assertEquals(builder.clear(), 5)
+        assertEquals(5, builder.clear())
+    }
+
+    @Test
+    fun testFloat32Encoding() {
+        val view = JvmPlatform.dataViewOf(ByteArray(256))
+        val builder = JbonBuilder(view)
+        // values -8 to 7 should be encoded in one byte
+        for (i in 0..15) {
+            val value = TINY_FLOATS[i]
+            builder.writeFloat(value)
+            assertEquals(TYPE_FLOAT4 xor i, view.getInt8(0).toInt() and 0xff)
+            assertEquals(1, builder.clear())
+        }
+        // all other values are encoded in 5 byte
+        builder.writeFloat(1.25f)
+        assertEquals(TYPE_FLOAT32, view.getInt8(0).toInt() and 0xff)
+        assertEquals(1.25f, view.getFloat32(1))
+        assertEquals(5, builder.clear())
+    }
+
+    @Test
+    fun testFloat64Encoding() {
+        val view = JvmPlatform.dataViewOf(ByteArray(256))
+        val builder = JbonBuilder(view)
+        // values -8 to 7 should be encoded in one byte
+        for (i in 0..15) {
+            val value = TINY_DOUBLES[i]
+            builder.writeDouble(value)
+            assertEquals(TYPE_FLOAT4 xor i, view.getInt8(0).toInt() and 0xff)
+            assertEquals(1, builder.clear())
+        }
+        // all other values are encoded in 5 byte
+        builder.writeDouble(1.25)
+        assertEquals(TYPE_FLOAT64, view.getInt8(0).toInt() and 0xff)
+        assertEquals(1.25, view.getFloat64(1))
+        assertEquals(9, builder.clear())
     }
 
     @Test
@@ -64,37 +128,37 @@ class JvmJbonTest {
         val builder = JbonBuilder(view)
         // a value between 0 and (including) 10 must result in one byte
         builder.writeSize32(0);
-        assertEquals(TYPE_SIZE32, view.getInt8(0).toInt())
-        assertEquals(builder.clear(), 1)
+        assertEquals(TYPE_SIZE32, view.getInt8(0).toInt() and 0xff)
+        assertEquals(1, builder.clear())
         builder.writeSize32(11);
-        assertEquals(11, view.getInt8(0).toInt() and 0xf)
-        assertEquals(builder.clear(), 1)
+        assertEquals(11, (view.getInt8(0).toInt() and 0xff) - TYPE_SIZE32)
+        assertEquals(1, builder.clear())
         // a value between 12 and 267 must be encoded in two byte
         builder.writeSize32(12);
-        assertEquals(TYPE_SIZE32 xor 12, view.getInt8(0).toInt())
+        assertEquals(TYPE_SIZE32 xor 12, view.getInt8(0).toInt() and 0xff)
         assertEquals(0, view.getInt8(1))
-        assertEquals(builder.clear(), 2)
+        assertEquals(2, builder.clear())
         builder.writeSize32(267);
-        assertEquals(TYPE_SIZE32 xor 12, view.getInt8(0).toInt())
-        assertEquals((267-12).toByte(), view.getInt8(1))
-        assertEquals(builder.clear(), 2)
+        assertEquals(TYPE_SIZE32 xor 12, view.getInt8(0).toInt() and 0xff)
+        assertEquals((267 - 12).toByte(), view.getInt8(1))
+        assertEquals(2, builder.clear())
         // a value between 268 and 65535 must be encoded in three byte
         builder.writeSize32(268);
-        assertEquals(TYPE_SIZE32 xor 13, view.getInt8(0).toInt())
+        assertEquals(TYPE_SIZE32 xor 13, view.getInt8(0).toInt() and 0xff)
         assertEquals(268, view.getInt16(1))
-        assertEquals(builder.clear(), 3)
+        assertEquals(3, builder.clear())
         builder.writeSize32(65535);
-        assertEquals(TYPE_SIZE32 xor 13, view.getInt8(0).toInt())
+        assertEquals(TYPE_SIZE32 xor 13, view.getInt8(0).toInt() and 0xff)
         assertEquals(65535.toShort(), view.getInt16(1))
-        assertEquals(builder.clear(), 3)
+        assertEquals(3, builder.clear())
         // a value above 65535 must be encoded in five byte
         builder.writeSize32(65536);
-        assertEquals(TYPE_SIZE32 xor 15, view.getInt8(0).toInt())
+        assertEquals(TYPE_SIZE32 xor 15, view.getInt8(0).toInt() and 0xff)
         assertEquals(65536, view.getInt32(1))
-        assertEquals(builder.clear(), 5)
+        assertEquals(5, builder.clear())
         builder.writeSize32(Integer.MAX_VALUE);
-        assertEquals(TYPE_SIZE32 xor 15, view.getInt8(0).toInt())
+        assertEquals(TYPE_SIZE32 xor 15, view.getInt8(0).toInt() and 0xff)
         assertEquals(Integer.MAX_VALUE, view.getInt32(1))
-        assertEquals(builder.clear(), 5)
+        assertEquals(5, builder.clear())
     }
 }
