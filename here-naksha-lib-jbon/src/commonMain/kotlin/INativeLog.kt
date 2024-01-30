858bf3777ebfbc8956ab38abb969ac786cdc19fb
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-01-30 21:22:33 +0100

    Add missing files.

---
+++ here-naksha-lib-jbon/src/commonMain/kotlin/INativeLog.kt
@@ -0,0 +1,27 @@
+@file:OptIn(ExperimentalJsExport::class)
+
+package com.here.naksha.lib.jbon
+
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
+
+/**
+ * An API to access the native logging.
+ */
+@JsExport
+interface INativeLog {
+    /**
+     * Writes an info.
+     */
+    fun info(msg:String, vararg args : Any)
+
+    /**
+     * Writes a warning.
+     */
+    fun warn(msg:String, vararg args:Any)
+
+    /**
+     * Writes an error.
+     */
+    fun error(msg:String, vararg args:Any)
+}
---
+++ here-naksha-lib-jbon/src/commonMain/kotlin/ISql.kt
@@ -0,0 +1,28 @@
+@file:OptIn(ExperimentalJsExport::class)
+
+package com.here.naksha.lib.jbon
+
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
+
+/**
+ * The API that grants access to native SQL.
+ */
+@JsExport
+interface ISql {
+    /**
+     * Execute an SQL query with the given arguments. The placeholder should be **$1** to **$n**.
+     * @param sql The SQL query to execute.
+     * @param args The arguments to be set at $n position, where $1 is the first array element.
+     * @return The result-set.
+     */
+    fun execute(sql: String, args: Array<Any>): ISqlResultSet
+
+    /**
+     * Prepare the given SQL statement using parameters of the given types.
+     * @param sql The SQL query to execute.
+     * @param typeNames The name of the types of the arguments, to be at $n position, where $1 is the first array element.
+     * @return The prepared plan.
+     */
+    fun prepare(sql: String, typeNames: Array<String>) : ISqlPlan
+}
