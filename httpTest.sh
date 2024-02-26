f9ce1e5fbe609851f98911310c61446eb5156395
Adamczyk, Tomasz <tomasz.adamczyk@here.com>
2024-02-26 15:07:06 +0100

    Test .sh deleted

--- httpTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Utility test script. Runs Http Storage tests. Do not merge to master
-psql postgres -c "drop schema naksha_data_schema cascade"
-gradle :here-naksha-app-service:test --tests 'com.here.naksha.app.service.ReadFeaturesByIdsHttpTest.*' --rerun-tasks
-# gradle :here-naksha-app-service:test --tests 'com.here.naksha.app.service.ReadFeaturesByIdsHttpTest.*' --rerun-tasks 2> /dev/null | grep -e 'Hello' -e 'Bye' -e 'BUILD'
