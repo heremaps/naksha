e5f1fea65aa4cf2b2e311951813887bbaf7b8905
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-03-21 09:07:29 +0100

    Add reverse hashing to FNV1a32

--- here-naksha-lib-jbon/src/commonMain/kotlin/Fnv1a32.kt
+++ here-naksha-lib-jbon/src/commonMain/kotlin/Fnv1a32.kt
@@ -32,10 +32,44 @@ object Fnv1a32 {
             var unicode: Int
             if (i < string.length && hi.isHighSurrogate()) {
                 val lo = string[i++]
-                require(lo.isLowSurrogate())
+                require(lo.isLowSurrogate()) {"Found invalid low surrogate at index ${i-1}"}
                 unicode = CodePoints.toCodePoint(hi, lo)
             } else {
                 unicode = hi.code
+            }
+            when (unicode) {
+                in 0..255 -> hash = int8(hash, unicode.toByte())
+                in 256..65535 -> hash = int16BE(hash, unicode.toShort())
+                in 65536..2_097_151 -> {
+                    hash = int8(hash, (unicode ushr 16).toByte())
+                    hash = int16BE(hash, unicode.toShort())
+                }
+
+                else -> throw IllegalArgumentException("Invalid unicode found: $unicode")
+            }
+        }
+        return hash
+    }
+
+    /**
+     * Hash the given string reverse (from back to front). Internally all code-points are hashed by their width, so 8-bit, 16-bit or
+     * 24-bit (unicode is never more than 21-bit) produce 1 to 3 hash calculations.
+     * @param hashCode The current hash code.
+     * @param string The string to hash.
+     * @return the updated hash-code.
+     */
+    fun stringReverse(hashCode: Int, string: String): Int {
+        var hash = hashCode
+        var i = string.length
+        while (--i >= 0) {
+            val lo = string[i]
+            var unicode: Int
+            if (i < string.length && lo.isLowSurrogate()) {
+                val hi = string[i++]
+                require(hi.isHighSurrogate()) {"Found invalid high surrogate at index ${i-1}"}
+                unicode = CodePoints.toCodePoint(hi, lo)
+            } else {
+                unicode = lo.code
             }
             when (unicode) {
                 in 0..255 -> hash = int8(hash, unicode.toByte())
