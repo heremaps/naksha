eda179993682f8ab4671fcade47bee80edb291d3
Adamczyk, Tomasz <tomasz.adamczyk@here.com>
2024-03-22 10:10:44 +0100

    MCPODS-7252 More tests

--- here-naksha-app-service/src/test/java/com/here/naksha/app/service/IterateFeaturesHttpStorageTest.java
+++ here-naksha-app-service/src/test/java/com/here/naksha/app/service/IterateFeaturesHttpStorageTest.java
@@ -23,6 +23,8 @@ import com.github.tomakehurst.wiremock.matching.UrlPathPattern;
 import com.here.naksha.app.common.ApiTest;
 import com.here.naksha.app.common.NakshaTestWebClient;
 import com.here.naksha.app.common.assertions.ResponseAssertions;
+import com.here.naksha.app.service.testutil.GzipUtil;
+import netscape.javascript.JSException;
 import org.json.JSONException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -49,7 +51,6 @@ class IterateFeaturesHttpStorageTest extends ApiTest {
   @BeforeAll
   static void setup() throws URISyntaxException, IOException, InterruptedException {
     setupSpaceAndRelatedResources(nakshaClient, TEST_DIR_PATH + "setup");
-
   }
 
   @Test
@@ -68,6 +69,85 @@ class IterateFeaturesHttpStorageTest extends ApiTest {
 
     // When: Iterate Features request is submitted to NakshaHub
     final HttpResponse<String> response = nakshaClient.get("hub/spaces/" + SPACE_ID + "/iterate" + "?" + limitQueryParam, streamId);
+
+    // Then: Perform assertions
+    ResponseAssertions.assertThat(response)
+            .hasStatus(200)
+            .hasStreamIdHeader(streamId)
+            .hasJsonBody(expectedBodyPart, "Get Feature response body doesn't match")
+            .hasNoNextPageToken();
+
+    verify(1, getRequestedFor(HTTP_STORAGE_URL_PATH));
+  }
+
+  @Test
+  void tc1200_testEmptyCollectionResponse() throws URISyntaxException, InterruptedException, IOException {
+    // Test API : GET /hub/spaces/{spaceId}/iterate
+    // Validate all features getting returned in single iteration without handle,
+    // when number of features are less than limit specified
+    // Given: iterate parameters
+    final String expectedBodyPart = loadFileOrFail(TEST_DIR_PATH + "TC1200_emptyCollectionResponse/iterate_response.json");
+    final String streamId = UUID.randomUUID().toString();
+
+    stubFor(get(HTTP_STORAGE_URL_PATH)
+            .willReturn(okJson(expectedBodyPart)));
+
+    // When: Iterate Features request is submitted to NakshaHub
+    final HttpResponse<String> response = nakshaClient.get("hub/spaces/" + SPACE_ID + "/iterate", streamId);
+
+    // Then: Perform assertions
+    ResponseAssertions.assertThat(response)
+            .hasStatus(200)
+            .hasStreamIdHeader(streamId)
+            .hasJsonBody(expectedBodyPart, "Get Feature response body doesn't match")
+            .hasFeatureCount(0)
+            .hasNoNextPageToken();
+
+    verify(1, getRequestedFor(HTTP_STORAGE_URL_PATH));
+  }
+
+  @Test
+  void tc1201_testLessFeaturesThanLimit() throws URISyntaxException, InterruptedException, IOException {
+    // Test API : GET /hub/spaces/{spaceId}/iterate
+    // Validate all features getting returned in single iteration without handle,
+    // when number of features are less than limit specified
+    // Given: iterate parameters
+    final String limitQueryParam = "limit=5";
+    final String expectedBodyPart = loadFileOrFail(TEST_DIR_PATH + "TC1201_lessFeaturesThanLimit/iterate_response.json");
+    final String streamId = UUID.randomUUID().toString();
+
+    // Given: response with 2 featuresq
+    stubFor(get(HTTP_STORAGE_URL_PATH)
+            .withQueryParam("limit", equalTo("5"))
+            .willReturn(okJson(expectedBodyPart)));
+
+    // When: Iterate Features request is submitted to NakshaHub
+    final HttpResponse<String> response = nakshaClient.get("hub/spaces/" + SPACE_ID + "/iterate" + "?" + limitQueryParam, streamId);
+
+    // Then: Perform assertions
+    ResponseAssertions.assertThat(response)
+            .hasStatus(200)
+            .hasStreamIdHeader(streamId)
+            .hasJsonBody(expectedBodyPart, "Get Feature response body doesn't match")
+            .hasFeatureCount(2)
+            .hasNoNextPageToken();
+
+    verify(1, getRequestedFor(HTTP_STORAGE_URL_PATH));
+  }
+
+  @Test
+  void tc1202_testGzipEncodedResponse() throws URISyntaxException, InterruptedException, IOException {
+    // Test API : GET /hub/spaces/{spaceId}/iterate
+    // Validate all features getting returned in single iteration without handle,
+    // when number of features are less than limit specified
+    // Given: iterate parameters
+    final String expectedBodyPart = loadFileOrFail(TEST_DIR_PATH + "TC1202_gzipEncodedResponse/iterate_response.json");
+    final String streamId = UUID.randomUUID().toString();
+
+    GzipUtil.stubOkGzipEncoded(get(HTTP_STORAGE_URL_PATH), expectedBodyPart);
+
+    // When: Iterate Features request is submitted to NakshaHub
+    final HttpResponse<String> response = nakshaClient.get("hub/spaces/" + SPACE_ID + "/iterate", streamId);
 
     // Then: Perform assertions
     ResponseAssertions.assertThat(response)
