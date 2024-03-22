bfb2b9c071e2f4e802226a441354af027215fc2d
phmai <phuc.mai@here.com>
2024-03-22 11:23:45 +0100

    fix wrong naming

--- here-naksha-app-service/src/test/java/com/here/naksha/app/service/ViewTypeUnionTest.java
+++ here-naksha-app-service/src/test/java/com/here/naksha/app/service/ViewTypeUnionTest.java
@@ -18,42 +18,42 @@ import static com.here.naksha.app.common.TestUtil.loadFileOrFail;
 import static com.here.naksha.app.common.assertions.ResponseAssertions.assertThat;
 
 @WireMockTest(httpPort = 9090)
-public class ViewTypeUnionAllTest extends ApiTest {
+public class ViewTypeUnionTest extends ApiTest {
 
     private static final NakshaTestWebClient nakshaClient = new NakshaTestWebClient();
 
-    private static final String HTTP_SPACE_ID = "base_view_union_all_test_http_space";
-    private static final String PSQL_SPACE_ID = "delta_view_union_all_test_psql_space";
-    private static final String VIEW_SPACE_ID = "view_union_all_view_space";
+    private static final String HTTP_SPACE_ID = "base_view_union_test_http_space";
+    private static final String PSQL_SPACE_ID = "delta_view_union_test_psql_space";
+    private static final String VIEW_SPACE_ID = "view_union_view_space";
 
-    private static final String ENDPOINT = "/view_union_all/test/bbox";
+    private static final String ENDPOINT = "/view_union/test/bbox";
 
 
     @BeforeAll
     static void setup() throws URISyntaxException, IOException, InterruptedException {
         // Set up Http Storage based Space
-        setupSpaceAndRelatedResources(nakshaClient, "ViewUnionAll/setup/http_storage_space");
+        setupSpaceAndRelatedResources(nakshaClient, "ViewUnion/setup/http_storage_space");
         // Set up (standard) Psql Storage based Space
-        setupSpaceAndRelatedResources(nakshaClient, "ViewUnionAll/setup/psql_storage_space");
+        setupSpaceAndRelatedResources(nakshaClient, "ViewUnion/setup/psql_storage_space");
         // Set up View Space over Psql and Http Storage based spaces
-        setupSpaceAndRelatedResources(nakshaClient, "ViewUnionAll/setup/view_space");
+        setupSpaceAndRelatedResources(nakshaClient, "ViewUnion/setup/view_space");
         // Load some test data in PsqlStorage based Space
-        final String initialFeaturesJson2 = loadFileOrFail("ViewUnionAll/setup/psql_storage_space/create_features.json");
+        final String initialFeaturesJson2 = loadFileOrFail("ViewUnion/setup/psql_storage_space/create_features.json");
         final HttpResponse<String> response2 = nakshaClient.post("hub/spaces/" + PSQL_SPACE_ID + "/features", initialFeaturesJson2, UUID.randomUUID().toString());
         assertThat(response2).hasStatus(200);
     }
 
     @Test
-    void testGetByBBoxOnViewSpaceUnionAll() throws Exception {
+    void testGetByBBoxOnViewSpaceUnion() throws Exception {
         // Test API : GET /hub/spaces/{spaceId}/bbox
         // Validate features returned match with given BBox condition using View space over psql and http storage based spaces
 
         // Given: Features By BBox request (against view space)
         final String bboxQueryParam = "west=12.79&south=53.59&east=12.82&north=53.62";
         final String httpStorageMockResponse =
-                loadFileOrFail("ViewUnionAll/ByBBox/http_storage_response.json");
+                loadFileOrFail("ViewUnion/ByBBox/http_storage_response.json");
         final String expectedViewResponse =
-                loadFileOrFail("ViewUnionAll/ByBBox/feature_response_part.json");
+                loadFileOrFail("ViewUnion/ByBBox/feature_response_part.json");
         String streamId = UUID.randomUUID().toString();
 
         final UrlPattern endpointPath = urlPathEqualTo(ENDPOINT);
