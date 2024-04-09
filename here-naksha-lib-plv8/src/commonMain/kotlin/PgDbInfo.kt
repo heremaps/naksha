ebe3b5b7f6bab458e79b3a443bb070a301f6c93b
Pawel Mazurek <52866094+cyberhead-pl@users.noreply.github.com>
2024-04-09 14:23:43 +0200

    PgDbInfo (#242)
    Co-authored-by: Pawel Mazurek <mazurek@J49L39Q5YW.ad.here.com>

--- here-naksha-lib-plv8/src/commonMain/kotlin/PgDbInfo.kt
+++ here-naksha-lib-plv8/src/commonMain/kotlin/PgDbInfo.kt
@@ -1,7 +1,12 @@
 package com.here.naksha.lib.plv8
 
+import com.here.naksha.lib.jbon.asMap
+import com.here.naksha.lib.jbon.get
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
+
+private const val MAX_POSTGRES_TOAST_TUPLE_TARGET = 32736
+private const val MIN_POSTGRES_TOAST_TUPLE_TARGET = 128
 
 /**
  * Information about the database, that need only to be queried ones per session.
@@ -12,8 +17,26 @@ import kotlin.js.JsExport
  */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
-class PgDbInfo(val sql: IPlv8Sql, val pageSize: Int = 8192, val brittleTableSpace: String? = null, val tempTableSpace: String? = null) {
-    init {
-        // TODO: Load the information from the session.
+class PgDbInfo(val sql: IPlv8Sql) {
+    val pageSize : Int by lazy {
+        val settings = asMap(sql.rows(sql.execute("SELECT current_setting('block_size') as bs"))!![0])
+        val blockSizeStr: String? = settings["bs"]
+        val blockSize = blockSizeStr?.toInt() ?: 8192
+        if (blockSize > MAX_POSTGRES_TOAST_TUPLE_TARGET) {
+            MAX_POSTGRES_TOAST_TUPLE_TARGET
+        } else if (blockSize < MIN_POSTGRES_TOAST_TUPLE_TARGET) {
+            MIN_POSTGRES_TOAST_TUPLE_TARGET
+        } else {
+            blockSize
+        }
     }
+    val brittleTableSpace: String? by lazy {
+        val rows = sql.rows(sql.execute("select oid from pg_tablespace where spcname = '$TEMPORARY_TABLESPACE';"))
+        if (rows.isNullOrEmpty()) {
+            null
+        } else {
+            TEMPORARY_TABLESPACE
+        }
+    }
+    val tempTableSpace: String? by lazy { brittleTableSpace }
 }
