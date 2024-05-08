1f9eeb58e4d63a2efc8bffb963becbba7c4bac5a
Joseph Lietz <joseph.lietz@here.com>
2024-05-08 09:01:29 -0500

    Version 3.0.0-alpha.15

--- here-naksha-lib-core/src/main/java/com/here/naksha/lib/core/NakshaVersion.java
+++ here-naksha-lib-core/src/main/java/com/here/naksha/lib/core/NakshaVersion.java
@@ -60,12 +60,14 @@ public class NakshaVersion implements Comparable<NakshaVersion> {
   public static final String v3_0_0_alpha_11 = "3.0.0-alpha.11";
   public static final String v3_0_0_alpha_12 = "3.0.0-alpha.12";
   public static final String v3_0_0_alpha_13 = "3.0.0-alpha.13";
+  public static final String v3_0_0_alpha_14 = "3.0.0-alpha.14";
+  public static final String v3_0_0_alpha_15 = "3.0.0-alpha.15";
 
   /**
    * The latest version of the naksha-extension stored in the resources.
    */
   @AvailableSince(v2_0_5)
-  public static final NakshaVersion latest = of(v3_0_0_alpha_13);
+  public static final NakshaVersion latest = of(v3_0_0_alpha_15);
 
   private final int major;
   private final int minor;
