93969f4c7a8615097affeee345f0544301fe74ea
lakde <rohit.lakde@here.com>
2024-05-22 08:46:31 -0500

    MCPODS-7650_Corrected log messages as per review.

--- docker/run-app.sh
+++ docker/run-app.sh
@@ -10,23 +10,19 @@ sed -i "s+SOME_S3_BUCKET+${NAKSHA_EXTENSION_S3_BUCKET}+g" /home/naksha/app/confi
 if [ -n "$NAKSHA_JWT_PVT_KEY" ]; then
     mkdir -p ${NAKSHA_CONFIG_PATH}auth/
     echo "$NAKSHA_JWT_PVT_KEY" | sed 's/\\n/\n/g' > ${NAKSHA_CONFIG_PATH}auth/jwt.key
-    echo "Created jwt.key"
+    echo "Using custom JWT private key"
 else
-    echo "NAKSHA_JWT_PVT_KEY is not set"
+    echo "No custom JWT private key supplied"
 fi
 
 # Check if NAKSHA_JWT_PUB_KEY is set and create jwt.pub file if it is
 if [ -n "$NAKSHA_JWT_PUB_KEY" ]; then
     mkdir -p ${NAKSHA_CONFIG_PATH}auth/
     echo "$NAKSHA_JWT_PUB_KEY" | sed 's/\\n/\n/g' > ${NAKSHA_CONFIG_PATH}auth/jwt.pub
-    echo "Created jwt.pub"
+    echo "Using custom JWT public key"
 else
-    echo "NAKSHA_JWT_PUB_KEY is not set"
+    echo "No custom JWT public key supplied"
 fi
-
-
-# TODO Remove below command
-cat $NAKSHA_CONFIG_PATH/cloud-config.json
 
 # Start the application
 java -jar /home/naksha/app/naksha-*-all.jar $NAKSHA_CONFIG_ID $NAKSHA_ADMIN_DB_URL
