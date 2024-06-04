fd0a7409b11a0db36d03c84096a8c87191c38d03
Pawel Mazurek <52866094+cyberhead-pl@users.noreply.github.com>
2024-06-04 16:16:02 +0200

    P_List (#282)
    * P_List
    
    * rev
    
    ---------
    
    Co-authored-by: Pawel Mazurek <mazurek@J49L39Q5YW.ad.here.com>

---
+++ here-naksha-lib-base/src/jvmTest/kotlin/P_ListTest.kt
@@ -0,0 +1,190 @@
+import com.here.naksha.lib.base.P_List
+import com.here.naksha.lib.base.Platform
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class P_ListTest {
+
+    @Test
+    fun shouldAddElements() {
+        // given
+        val list = P_List(Platform.intKlass)
+
+        // when
+        list.add(11)
+        // then
+        assertEquals(11, list[0])
+
+        // when
+        list.add(null)
+        // then
+        assertEquals(null, list[1])
+    }
+
+    @Test
+    fun shouldSetElementAtIndex() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(11)
+        list.add(12)
+        list.add(13)
+
+        // when
+        list.add(1, 7)
+
+        // then
+        assertEquals(11, list[0])
+        assertEquals(7, list[1])
+        assertEquals(12, list[2])
+        assertEquals(13, list[3])
+    }
+
+    @Test
+    fun testAddIndexOutOfBounds() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(11)
+
+        // expect
+        assertThrows<RuntimeException> { list.add(-1, 12) }
+        assertThrows<RuntimeException> { list.add(13, 12) }
+    }
+
+    @Test
+    fun testListSize() {
+        // given
+        val list = P_List(Platform.stringKlass)
+        // expect
+        assertEquals(0, list.size)
+
+        // when
+        list.add("any")
+
+        // then
+        assertEquals(1, list.size)
+    }
+
+    @Test
+    fun testAddAll() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+
+        // when
+        list.addAll(listOf(10, 11))
+
+        // then
+        assertEquals(3, list.size)
+        assertEquals(10, list[1])
+        assertEquals(11, list[2])
+    }
+
+    @Test
+    fun testAddAllAtIndex() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+
+        // when
+        list.addAll(1, listOf(10, 11))
+
+        // then
+        assertEquals(3, list.size)
+        assertEquals(10, list[1])
+        assertEquals(11, list[2])
+        assertEquals(8, list[3])
+    }
+
+    @Test
+    fun testContainsAll() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+        list.add(9)
+
+        // expect
+        assertTrue(list.containsAll(listOf(7)))
+        assertTrue(list.containsAll(listOf(7, 9)))
+        assertTrue(list.containsAll(listOf(7, 8, 9)))
+        assertFalse(list.containsAll(listOf(5, 9)))
+    }
+
+    @Test
+    fun testRemoveAll() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+        list.add(9)
+
+        // when
+        val result = list.removeAll(listOf(8, 9))
+
+        // then
+        assertTrue(result)
+        assertEquals(1, list.size)
+        assertEquals(7, list[0])
+    }
+
+    @Test
+    fun testRetainAll() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+        list.add(9)
+
+        // when
+        val result = list.retainAll(listOf(8, 9))
+
+        // then
+        assertTrue(result)
+        assertEquals(2, list.size)
+        assertEquals(8, list[0])
+        assertEquals(9, list[1])
+    }
+
+    @Test
+    fun testSubList() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+        list.add(9)
+        list.add(10)
+
+        // when
+        val result = list.subList(1, 3)
+
+        // then
+        assertEquals(2, result.size)
+        assertEquals(4, list.size)
+        assertEquals(8, result[0])
+        assertEquals(9, result[1])
+
+        // when
+        val result2 = list.subList(2, 2)
+        assertTrue(result2.isEmpty())
+    }
+
+    @Test
+    fun testMutableIterator() {
+        // given
+        val list = P_List(Platform.intKlass)
+        list.add(7)
+        list.add(8)
+
+        // when
+        val iterator = list.listIterator()
+
+        // then
+        assertEquals(7, iterator.next())
+        assertEquals(8, iterator.next())
+        assertThrows<NoSuchElementException> { iterator.next() }
+    }
+}
