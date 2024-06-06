dcf2690d8e5ed7abd70b78a62867e404b1f1c8aa
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-06-06 11:34:28 +0200

    Fix tests (P_List functions are missing).

--- here-naksha-lib-base/src/jvmMain/kotlin/JvmMapKeyIterator.kt
+++ here-naksha-lib-base/src/jvmMain/kotlin/JvmMapKeyIterator.kt
@@ -1,7 +1,7 @@
 package com.here.naksha.lib.base
 
 internal class JvmMapKeyIterator(map: PlatformMap?) : PlatformIterator<Any>() {
-    private val it: MutableIterator<Any>? = if (map is JvmMap) map.entrySet().iterator() else null
+    private val it: MutableIterator<Any>? = if (map is JvmMap) map.keySet().iterator() else null
     private val result: PlatformIteratorResult<Any> = PlatformIteratorResult(true, null)
     override fun next(): PlatformIteratorResult<Any> {
         if (it?.hasNext() == true) {
