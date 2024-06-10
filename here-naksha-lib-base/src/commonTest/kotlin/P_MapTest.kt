0d7c71b65f4223e162ea580195ccfd6877bf7ef5
Jakub Amanowicz <jakub.amanowicz@here.com>
2024-06-10 11:12:40 +0200

    missing p_map functions (#295)

--- here-naksha-lib-base/src/commonTest/kotlin/P_MapTest.kt
+++ here-naksha-lib-base/src/commonTest/kotlin/P_MapTest.kt
@@ -78,4 +78,99 @@ class P_MapTest {
             assertTrue(next.done)
         }
     }
+
+    @Test
+    fun testEntries() {
+        // Given: standard kotlin map
+        val stdMap: Map<String, String?> = mapOf(
+            "one" to "A",
+            "two" to "B",
+            "three" to "C"
+        )
+
+        // And: P_Map with items from stdMap
+        val pMap = TestMapStringString().apply { putAll(stdMap) }
+
+        // When: getting entry set
+        val entries = pMap.entries
+
+        // Then
+        assertEquals(stdMap.entries.size, entries.size)
+        entries.forEach { (key, value) ->
+            assertEquals(stdMap[key], value)
+        }
+    }
+
+    @Test
+    fun testKeys() {
+        // Given: a P_Map
+        val pMap = pMap(
+            "one" to "A",
+            "two" to "B",
+            "three" to "C"
+        )
+
+        // When: getting key set
+        val keys = pMap.keys
+
+        // Then
+        assertEquals(3, keys.size)
+        assertTrue {
+            keys.containsAll(listOf("one", "two", "three"))
+        }
+    }
+
+    @Test
+    fun testValues() {
+        // Given: a P_Map
+        val pMap = pMap(
+            "one" to "A",
+            "two" to "B",
+            "three" to "C"
+        )
+
+        // When: getting key set
+        val values = pMap.values
+
+        // Then
+        assertEquals(3, values.size)
+        assertTrue {
+            values.containsAll(listOf("A", "B", "C"))
+        }
+    }
+
+    @Test
+    fun testPutAll() {
+        // Given: a P_Map
+        val firstMap = pMap(
+            "one" to "A",
+            "two" to "B",
+            "three" to "C"
+        )
+
+        // And:
+        val secondMap = pMap(
+            "two" to "X",
+            "four" to "Y"
+        )
+
+        // When
+        firstMap.putAll(secondMap)
+
+        // Then
+        assertEquals(4, firstMap.size)
+        listOf(
+            "one" to "A",
+            "two" to "X",
+            "three" to "C",
+            "four" to "Y"
+        ).forEach { (key, value) ->
+            assertEquals(firstMap[key], value)
+        }
+    }
+
+    private fun pMap(vararg keyPairs: Pair<String, String>): P_Map<String, String> {
+        val stdMap: Map<String, String?> = mapOf(*keyPairs)
+        return TestMapStringString().apply { putAll(stdMap) }
+    }
 }
