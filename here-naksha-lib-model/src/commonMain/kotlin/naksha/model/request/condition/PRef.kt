3584eb14a1f0a8f26ba8c4c158f5a46f2fb534cd
Pawel Mazurek <mazurek@J49L39Q5YW.ad.here.com>
2024-06-25 14:04:37 +0200

    PRef for java usage

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/request/condition/PRef.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/request/condition/PRef.kt
@@ -2,6 +2,7 @@ package naksha.model.request.condition
 
 import kotlin.js.ExperimentalJsExport
 import kotlin.js.JsExport
+import kotlin.jvm.JvmStatic
 
 /**
  * All property operations.
@@ -18,4 +19,22 @@ sealed class PRef {
     data object TXN_NEXT: PRef()
     data object TAGS: PRef()
     class NON_INDEXED_PREF(vararg val path: String): PRef()
+
+    companion object{
+
+        /**
+         * Just for convenient usage in java
+         */
+
+        @JvmStatic fun id() = ID
+        @JvmStatic fun appId() = APP_ID
+        @JvmStatic fun author() = AUTHOR
+        @JvmStatic fun uid() = UID
+        @JvmStatic fun grid() = GRID
+        @JvmStatic fun txn() = TXN
+        @JvmStatic fun txnNext() = TXN_NEXT
+        @JvmStatic fun tags() = TAGS
+        @JvmStatic fun nonIndexedPref(vararg path: String) = NON_INDEXED_PREF(*path)
+    }
+
 }
