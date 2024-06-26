cc15efbe7e928e3d7285be16d5828f75fd1331ce
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-06-26 11:33:40 +0200

    Fix Core-Context to use the new standard lambdas.

--- here-naksha-lib-core/src/main/java/com/here/naksha/lib/core/NakshaCoreContext.java
+++ here-naksha-lib-core/src/main/java/com/here/naksha/lib/core/NakshaCoreContext.java
@@ -1,6 +1,10 @@
 package com.here.naksha.lib.core;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import kotlin.Unit;
+import naksha.base.fn.Fn0;
+import naksha.base.fn.Fn1;
+import naksha.base.fn.Fx1;
 import naksha.model.NakshaContext;
 import naksha.model.NakshaVersion;
 import org.jetbrains.annotations.ApiStatus.AvailableSince;
@@ -12,7 +16,7 @@ import org.jetbrains.annotations.NotNull;
 public class NakshaCoreContext extends NakshaContext {
   private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
   @SuppressWarnings("NotNullFieldNotInitialized")
-  private static @NotNull NakshaContext.Fn _super;
+  private static @NotNull Fn0<NakshaContext> _super;
 
   /**
    * Must be invoked when the application using the core-library starts to bind this context as default context.
@@ -35,7 +39,7 @@ public class NakshaCoreContext extends NakshaContext {
   @AvailableSince(NakshaVersion.v2_0_5)
   public static @NotNull NakshaCoreContext currentContext() {
     final AbstractTask<?, ?> task = AbstractTask.currentTask();
-    return task != null ? (NakshaCoreContext) task.context() : (NakshaCoreContext) _super.nakshaContext();
+    return task != null ? (NakshaCoreContext) task.context() : (NakshaCoreContext) _super.call();
   }
 
 }
