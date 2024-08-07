0d8aadade62ba54bbd328e3db5302880b2d8182d
phmai <phuc.mai@here.com>
2024-08-07 12:36:45 +0200

    PR comments

--- here-naksha-lib-core/src/test/java/com/here/naksha/lib/core/JsonMappingTest.java
+++ here-naksha-lib-core/src/test/java/com/here/naksha/lib/core/JsonMappingTest.java
@@ -75,7 +75,7 @@ public class JsonMappingTest {
     final String json =
         "{\"type\":\"ErrorResponse\",\"error\":\"NotImplemented\",\"errorMessage\":\"Hello World!\"}";
     JvmObject jvmMap = (JvmObject) Platform.fromJSON(json, FromJsonOptions.getDEFAULT());
-    //TODO
+    //TODO(lib-core test)
 //    final ErrorResponse obj = jvmMap.proxy(Platform.klassOf(ErrorResponse.class));
 //    assertNotNull(obj);
 //    assertSame(NakshaErrorCode.NOT_IMPLEMENTED, obj.error.code);
