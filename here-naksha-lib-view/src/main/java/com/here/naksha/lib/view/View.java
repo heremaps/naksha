d2537dd676a6e933940846b0d9e8d1acffb855c3
phmai <phuc.mai@here.com>
2024-08-26 15:16:43 +0200

    partial fix

--- here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/View.java
+++ here-naksha-lib-view/src/main/java/com/here/naksha/lib/view/View.java
@@ -18,17 +18,11 @@
  */
 package com.here.naksha.lib.view;
 
-import com.here.naksha.lib.core.models.naksha.Storage;
-import java.util.Map;
-import naksha.base.Int64;
 import naksha.model.*;
-import naksha.model.objects.NakshaFeature;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class View implements IView {
-
-  private Storage storage;
+public class View {
 
   private ViewLayerCollection viewLayerCollection;
 
@@ -36,94 +30,19 @@ public class View implements IView {
     this.viewLayerCollection = viewLayerCollection;
   }
 
-  public View(Storage storage) {
-    this.storage = storage;
-  }
-
-  @Override
   public ViewLayerCollection getViewCollection() {
     return viewLayerCollection;
   }
 
-  @Override
   public @NotNull ViewReadSession newReadSession(@Nullable SessionOptions options) {
     return new ViewReadSession(this, options);
   }
 
-  @Override
   public @NotNull ViewWriteSession newWriteSession(@Nullable SessionOptions options) {
     return new ViewWriteSession(this, options);
   }
 
-  @Override
   public void setViewLayerCollection(ViewLayerCollection viewLayerCollection) {
     this.viewLayerCollection = viewLayerCollection;
-  }
-
-  // TODO implement all below methods
-  @Override
-  public void initStorage(@Nullable Map<String, ?> params) {
-    //    storage.init();
-  }
-
-  @NotNull
-  @Override
-  public ILock enterLock(@NotNull String id, @NotNull Int64 waitMillis) {
-    return null;
-  }
-
-  @Override
-  public void close() {}
-
-  @NotNull
-  @Override
-  public Tuple featureToRow(@NotNull NakshaFeature feature) {
-    return null;
-  }
-
-  @NotNull
-  @Override
-  public NakshaFeature rowToFeature(@NotNull Tuple tuple) {
-    return null;
-  }
-
-  @Nullable
-  @Override
-  public String getMapId(int mapNumber) {
-    return "";
-  }
-
-  @Override
-  public boolean contains(@NotNull String mapId) {
-    return false;
-  }
-
-  @NotNull
-  @Override
-  public IMap get(@NotNull String mapId) {
-    return null;
-  }
-
-  @NotNull
-  @Override
-  public IMap getDefaultMap() {
-    return null;
-  }
-
-  @Override
-  public boolean isInitialized() {
-    return false;
-  }
-
-  @NotNull
-  @Override
-  public SessionOptions getAdminOptions() {
-    return null;
-  }
-
-  @NotNull
-  @Override
-  public String getId() {
-    return "";
   }
 }
