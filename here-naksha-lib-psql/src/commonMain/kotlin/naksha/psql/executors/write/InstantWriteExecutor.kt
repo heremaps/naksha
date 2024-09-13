0abe0ee6267ff15e58d306c0058a3d04cff25060
Pawel Mazurek <52866094+cyberhead-pl@users.noreply.github.com>
2024-09-13 15:38:53 +0200

    fix sql injection

--- here-naksha-lib-psql/src/commonMain/kotlin/naksha/psql/executors/write/InstantWriteExecutor.kt
+++ here-naksha-lib-psql/src/commonMain/kotlin/naksha/psql/executors/write/InstantWriteExecutor.kt
@@ -18,7 +18,8 @@ class InstantWriteExecutor(
             val quotedIdColumn = quoteIdent(PgColumn.id.name)
             session.usePgConnection()
                 .execute(
-                    sql = "DELETE FROM $quotedDelTable WHERE $quotedIdColumn='$featureId'"
+                    sql = "DELETE FROM $quotedDelTable WHERE $quotedIdColumn=$1",
+                    args = arrayOf(featureId)
                 )
         }
     }
