afcdd4a1050a793fbaeb1e4d5d7773a72a7cc28b
phmai <phuc.mai@here.com>
2024-09-26 15:11:03 +0200

    support for more ops

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/request/PropertyFilter.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/request/PropertyFilter.kt
@@ -1,17 +1,17 @@
 package naksha.model.request
 
+import naksha.base.Platform
 import naksha.jbon.JbFeatureDecoder
 import naksha.model.request.query.*
 
 class PropertyFilter(val req: ReadFeatures) : ResultFilter {
 
+    companion object {
+        const val PROPERTIES = "properties"
+    }
+
     override fun call(a1: ResultTuple): ResultTuple? {
         val pSearch = req.query.properties ?: return a1
-        // Now, implement in Java the different operations supported
-        // In the tuple, you use the JbFeatureDecoder, not decode the feature!
-        // The feature decoder allow to map arbitrary bytes, like the feature bytes from the tuple
-        // decoder.mapBytes(tuple.feature)
-        // decoder.get("properties", "name")
         if (a1.tuple == null) return null
         if (a1.tuple!!.feature == null) return null
         val decoder = JbFeatureDecoder()
@@ -37,8 +37,33 @@ class PropertyFilter(val req: ReadFeatures) : ResultFilter {
             }
             is PNot -> return !resolvePropsQuery(pQuery, decoder)
             is PQuery -> {
-                return decoder[pQuery.property.name] == pQuery.value
+                val propFromFeature = decoder[PROPERTIES,pQuery.property.path.first()!!]
+                val op = pQuery.op
+                return resolveEachOp(op,propFromFeature,pQuery.value)
             }
+        }
+        return false
+    }
+
+    private fun resolveEachOp(op: AnyOp, featureProperty: Any?, queryProperty: Any?) : Boolean {
+        when (op) {
+            AnyOp.EXISTS -> return featureProperty != Platform.UNDEFINED
+            AnyOp.IS_NULL -> return featureProperty == null
+            AnyOp.IS_NOT_NULL -> return featureProperty != null
+            AnyOp.IS_TRUE -> return featureProperty == true
+            AnyOp.IS_FALSE -> return featureProperty == false
+            AnyOp.IS_ANY_OF -> {
+                if (queryProperty !is Array<*>) return false
+                return queryProperty.contains(featureProperty)
+            }
+            AnyOp.CONTAINS -> {}
+            StringOp.EQUALS -> return (featureProperty is String) && (queryProperty is String) && (featureProperty.toString() == queryProperty.toString())
+            StringOp.STARTS_WITH -> return (featureProperty is String) && (queryProperty is String) && (featureProperty.startsWith(queryProperty.toString()))
+            DoubleOp.EQ -> return (featureProperty is Number) && (queryProperty is Number) && (featureProperty.toDouble().equals(queryProperty.toDouble()))
+            DoubleOp.GT -> return (featureProperty is Number) && (queryProperty is Number) && (featureProperty.toDouble() > queryProperty.toDouble())
+            DoubleOp.LT -> return (featureProperty is Number) && (queryProperty is Number) && (featureProperty.toDouble() < queryProperty.toDouble())
+            DoubleOp.GTE -> return (featureProperty is Number) && (queryProperty is Number) && (featureProperty.toDouble() >= queryProperty.toDouble())
+            DoubleOp.LTE -> return (featureProperty is Number) && (queryProperty is Number) && (featureProperty.toDouble() <= queryProperty.toDouble())
         }
         return false
     }
