378721ed7dbe45ed00275aac2c5406a45192594f
adamczyk-HERE <tomasz.adamczyk@here.com>
2024-10-02 11:35:14 +0200

    CASL-533 StreamId tracking added
    Signed-off-by: adamczyk-HERE <tomasz.adamczyk@here.com>

--- here-naksha-storage-http/src/main/java/com/here/naksha/storage/http/ffw/FfwInterfaceReadExecute.java
+++ here-naksha-storage-http/src/main/java/com/here/naksha/storage/http/ffw/FfwInterfaceReadExecute.java
@@ -39,8 +39,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class FfwInterfaceReadExecute {
 
