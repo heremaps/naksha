70e356771162fa0b7bf84814fd10eb4eee8d7ae8
Alexander Lowey-Weber <alweber@J0PQJC3V6V.ad.here.com>
2024-10-02 11:40:27 +0200

    Adjust tags encoding to use the same as feature.

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagsEncoding.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagsEncoding.kt
@@ -9,24 +9,24 @@ import kotlin.js.JsExport
 @JsExport
 object TagsEncoding : FlagsBits() {
     /**
-     * A standard [JSON](https://datatracker.ietf.org/doc/html/rfc8259) encoded value.
-     */
-    const val JSON = 0 shl TAGS_SHIFT
-
-    /**
-     * A standard [JSON](https://datatracker.ietf.org/doc/html/rfc8259) encoded value, compressed using
-     * [GZIP](https://en.wikipedia.org/wiki/Gzip).
-     */
-    const val JSON_GZIP = 1 shl TAGS_SHIFT
-
-    /**
      * A JSON encoded using [JBON](https://github.com/heremaps/naksha/blob/v3/docs/JBON.md) encoding.
      */
-    const val JBON = 2 shl TAGS_SHIFT
+    const val JBON = 0 shl TAGS_SHIFT
 
     /**
      * A JSON encoded using [JBON](https://github.com/heremaps/naksha/blob/v3/docs/JBON.md) encoding, compressed using
      * [GZIP](https://en.wikipedia.org/wiki/Gzip).
      */
-    const val JBON_GZIP = 3 shl TAGS_SHIFT
+    const val JBON_GZIP = 1 shl TAGS_SHIFT
+
+    /**
+     * A standard [JSON](https://datatracker.ietf.org/doc/html/rfc8259) encoded value.
+     */
+    const val JSON = 2 shl TAGS_SHIFT
+
+    /**
+     * A standard [JSON](https://datatracker.ietf.org/doc/html/rfc8259) encoded value, compressed using
+     * [GZIP](https://en.wikipedia.org/wiki/Gzip).
+     */
+    const val JSON_GZIP = 3 shl TAGS_SHIFT
 }
