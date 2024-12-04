284dbed87da7bf6ce00f6f5aad1ee87377203764
Pawel Mazurek <52866094+cyberhead-pl@users.noreply.github.com>
2024-12-04 13:10:42 +0100

    CASL-656 NakshaException analyzer issue
    Signed-off-by: Pawel Mazurek <52866094+cyberhead-pl@users.noreply.github.com>

---
+++ here-naksha-lib-model/src/jvmMain/kotlin/naksha/model/NakshaException.kt
@@ -0,0 +1,14 @@
+package naksha.model
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual class NakshaException(@JvmField val error: NakshaError) : RuntimeException(error.msg, error.cause) {
+    /**
+     * Create an exception with error details individually specified.
+     * @param code the error code.
+     * @param msg the human-readable error message.
+     * @param id the optional identifier related to the error; if any.
+     * @param cause the cause (exception) of this error; if any.
+     * @since 3.0.0
+     */
+    constructor(code: String, msg: String, id: String? = null, cause: Throwable? = null) : this(NakshaError(code, msg, id, cause))
+}
