96c8649bfb6a6892d3357d4c729681af071e569e
phmai <phuc.mai@here.com>
2025-01-17 15:38:50 +0100

    gradle
    Signed-off-by: phmai <phuc.mai@here.com>

--- here-naksha-handler-activitylog/build.gradle.kts
+++ here-naksha-handler-activitylog/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     implementation(project(":here-naksha-lib-core"))
     implementation(project(":here-naksha-lib-psql"))
     implementation(project(":here-naksha-lib-handlers"))
+    implementation(project(":here-naksha-lib-diff"))
 
     implementation(Lib.flipkart_zjsonpatch)
     testImplementation(Lib.jayway_jsonpath)
