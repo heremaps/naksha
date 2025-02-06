4e2c3284dfda7965d081a5ee26bd6034ddfd9b7b
Alexander Lowey-weber <alweber@WJ195N7FQR.ad.here.com>
2025-02-06 10:23:34 +0100

    Refactor JvmStorage into PsqlStorage.

---
+++ here-naksha-lib-psql/src/jvmMain/kotlin/naksha/psql/PsqlStorage.kt
@@ -0,0 +1,59 @@
+package naksha.psql
+
+import naksha.base.fn.Fx2
+import naksha.jbon.JbDictionary
+import naksha.model.*
+import naksha.model.NakshaError.NakshaErrorCompanion.ILLEGAL_STATE
+import naksha.model.NakshaError.NakshaErrorCompanion.UNINITIALIZED
+import kotlin.reflect.KClass
+
+/**
+ * The Java implementation of the [PgStorage], classname `naksha.psql.JvmPgStorage`.
+ *
+ * @constructor Creates a new PSQL storage.
+ * @property cluster the PostgresQL cluster used by this storage.
+ * @param defaultSchemaName the default schema name.
+ */
+open class JvmPgStorage : PgStorage(), IStorage {
+
+    override val configKlass: KClass<PgConfig> = PgConfig::class
+
+    override fun afterInit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun shutdownStorage(dropCache: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    private var _channel: String? = null
+
+    /**
+     * The name of the notification channel used.
+     *
+     * - Will throw [UNINITIALIZED] if read before [initStorage].
+     * - Will throw [ILLEGAL_STATE] if change after [initStorage]
+     */
+    var channel: String
+        get() = _channel ?: throw NakshaException(UNINITIALIZED, "Storage uninitialized")
+        private set(value) {
+            _channel = value
+        }
+    private lateinit var listener: PsqlStorageListener
+
+    override fun initAdminMap(config: PgConfig, create: Boolean?, upgrade: Boolean?): PgAdminMap {
+        return JvmPgAdminMap(this, config, create, upgrade)
+    }
+
+    override fun newSession(options: SessionOptions, readOnly: Boolean): PsqlSession = PsqlSession(this, options, readOnly)
+    override fun newConnection(options: SessionOptions, readOnly: Boolean, init: Fx2<PgConnection, String>?): PgConnection {
+        TODO("Not yet implemented")
+    }
+
+    override fun adminConnection(): PgConnection {
+        TODO("Not yet implemented")
+    }
+    override fun getDictionary(id: String): JbDictionary? {
+        TODO("Not yet implemented")
+    }
+}
