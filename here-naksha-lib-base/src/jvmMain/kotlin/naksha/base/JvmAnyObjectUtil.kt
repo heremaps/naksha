dcbea9dbf4daafbd7cb4114904f8565078a3aea2
Jakub Amanowicz <jakub.amanowicz@here.com>
2025-02-10 10:22:49 +0100

    util, AccessLog
    Signed-off-by: Jakub Amanowicz <jakub.amanowicz@here.com>

--- here-naksha-lib-base/src/jvmMain/kotlin/naksha/base/JvmAnyObjectUtil.kt
+++ here-naksha-lib-base/src/jvmMain/kotlin/naksha/base/JvmAnyObjectUtil.kt
@@ -1,5 +1,6 @@
 package naksha.base
 
+import naksha.base.fn.Fn2
 import kotlin.jvm.internal.Reflection
 
 object JvmAnyObjectUtil {
@@ -7,4 +8,13 @@ object JvmAnyObjectUtil {
     @JvmStatic
     fun <T> getProperty(owner: AnyObject, key: String, type: Class<T>): T? =
         owner.getAs(key, Reflection.getOrCreateKotlinClass(type)) as T?
+
+    @JvmStatic
+    @JvmOverloads
+    fun <T> getOrCreateProperty(
+        owner: AnyObject,
+        key: String,
+        type: Class<T>,
+        init: Fn2<out T?, in AnyObject, in String>? = null
+    ): T = owner.getOrCreate(key, Reflection.getOrCreateKotlinClass(type), init) as T
 }
