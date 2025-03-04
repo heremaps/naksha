17906b96f02d7117c193dfdb67b96c644c52b31d
Alexander Lowey-weber <alweber@WJ195N7FQR.ad.here.com>
2025-03-04 13:23:31 +0100

    Fix tests that were still using hashId method

--- here-naksha-lib-model/src/commonTest/kotlin/naksha/model/PlatformTest.kt
+++ here-naksha-lib-model/src/commonTest/kotlin/naksha/model/PlatformTest.kt
@@ -1,7 +1,6 @@
 package naksha.model
 
 import naksha.model.Naksha.NakshaCompanion.featureNumber
-import naksha.model.Naksha.NakshaCompanion.hashId
 import naksha.model.Naksha.NakshaCompanion.partitionNumber
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -12,11 +11,11 @@ class PlatformTest {
     fun testPartitionNumber() {
         // expect
         val collectionPartitions = 128
-        assertEquals(44, partitionNumber(featureNumber(hashId("foo"))) % collectionPartitions)
-        assertEquals(127, partitionNumber(featureNumber(hashId("fooA"))) % collectionPartitions)
-        assertEquals(19, partitionNumber(featureNumber(hashId("fooB"))) % collectionPartitions)
-        assertEquals(39, partitionNumber(featureNumber(hashId("fooC"))) % collectionPartitions)
-        assertEquals(70, partitionNumber(featureNumber(hashId("fooD"))) % collectionPartitions)
+        assertEquals(44, partitionNumber(featureNumber("foo")) % collectionPartitions)
+        assertEquals(127, partitionNumber(featureNumber("fooA")) % collectionPartitions)
+        assertEquals(19, partitionNumber(featureNumber("fooB")) % collectionPartitions)
+        assertEquals(39, partitionNumber(featureNumber("fooC")) % collectionPartitions)
+        assertEquals(70, partitionNumber(featureNumber("fooD")) % collectionPartitions)
     }
 
 }
