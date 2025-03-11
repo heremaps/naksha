74532668f8fcb23ca421a5d155d2abb339b03989
Alexander Lowey-weber <alweber@WJ195N7FQR.ad.here.com>
2025-03-11 11:16:47 +0100

    Fix partition-number test

--- here-naksha-lib-model/src/commonTest/kotlin/naksha/model/PlatformTest.kt
+++ here-naksha-lib-model/src/commonTest/kotlin/naksha/model/PlatformTest.kt
@@ -11,11 +11,11 @@ class PlatformTest {
     fun testPartitionNumber() {
         // expect
         val collectionPartitions = 128
-        assertEquals(44, partitionNumber(featureNumber("foo")) % collectionPartitions)
-        assertEquals(127, partitionNumber(featureNumber("fooA")) % collectionPartitions)
-        assertEquals(19, partitionNumber(featureNumber("fooB")) % collectionPartitions)
-        assertEquals(39, partitionNumber(featureNumber("fooC")) % collectionPartitions)
-        assertEquals(70, partitionNumber(featureNumber("fooD")) % collectionPartitions)
+        assertEquals(88, partitionNumber(featureNumber("foo")) % collectionPartitions)
+        assertEquals(99, partitionNumber(featureNumber("fooA")) % collectionPartitions)
+        assertEquals(58, partitionNumber(featureNumber("fooB")) % collectionPartitions)
+        assertEquals(72, partitionNumber(featureNumber("fooC")) % collectionPartitions)
+        assertEquals(12, partitionNumber(featureNumber("fooD")) % collectionPartitions)
     }
 
 }
