21dd1029f100bb848f2131da445a9bdf271ef212
Alexander Lowey-Weber <alexander.weber@here.com>
2025-03-12 16:54:58 +0100

    Add a test that loads simply all features from a collection

---
+++ here-naksha-lib-psql/src/commonTest/kotlin/naksha/psql/ReadFeaturesAll.kt
@@ -0,0 +1,53 @@
+package naksha.psql
+
+import naksha.model.Action
+import naksha.model.objects.NakshaCollection
+import naksha.model.objects.NakshaFeature
+import naksha.model.request.*
+import naksha.psql.base.PgTestBase
+import naksha.psql.util.ProxyFeatureGenerator.generateRandomFeatures
+import kotlin.test.*
+
+class ReadFeaturesAll : PgTestBase(NakshaCollection("read_features_all")) {
+
+    companion object ReadFeaturesAll_C {
+        private const val COUNT = 100
+        private val allFeatures: MutableMap<String, NakshaFeature> = mutableMapOf()
+    }
+
+    @BeforeTest
+    fun prepareTestData() {
+        // Create random features
+        val featuresToCreate = generateRandomFeatures(COUNT)
+        val writeFeaturesReq = WriteRequest().apply {
+            featuresToCreate.forEach { featureToCreate ->
+                add(Write().createFeature(collection.mapId, collection.id, featureToCreate))
+            }
+        }
+        val writeFeaturesResp = executeWrite(writeFeaturesReq)
+        assertEquals(COUNT, writeFeaturesResp.features.size)
+        for (feature in writeFeaturesResp.features) {
+            assertNotNull(feature)
+            assertNull(allFeatures[feature.id])
+            assertEquals(Action.CREATED, feature.properties.xyz.action)
+            allFeatures[feature.id] = feature
+        }
+    }
+
+    @Test
+    fun shouldReturnAllFeatures() {
+        executeRead(ReadFeatures().apply {
+            mapId = collection.mapId
+            collectionIds += collection.id
+        }).apply {
+            assertEquals(COUNT, features.size)
+            for (feature in features) {
+                assertNotNull(feature)
+                assertNotNull(allFeatures[feature.id])
+                allFeatures.remove(feature.id)
+            }
+            assertEquals(0, allFeatures.size)
+        }
+    }
+
+}
