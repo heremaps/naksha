a66596658625cbc7720bb7858852292042fe7cbd
Alexander Lowey-Weber <alexander.weber@here.com>
2025-03-19 13:24:09 +0100

    Fix paco (gzip) to be strict, so it behaves like native C extension
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-psql/src/jvmMain/resources/pako.sql
+++ here-naksha-lib-psql/src/jvmMain/resources/pako.sql
@@ -4,38 +4,53 @@
 DO $sql$ BEGIN
   IF NOT EXISTS (SELECT name FROM pg_available_extensions WHERE name='gzip' AND installed_version IS NOT NULL) THEN
     EXECUTE $outer$
-      CREATE OR REPLACE FUNCTION gzip(uncompressed BYTEA, compression_level INTEGER) RETURNS BYTEA AS $inner$
+      CREATE OR REPLACE FUNCTION gzip(uncompressed BYTEA, compression_level INTEGER) RETURNS BYTEA
+      LANGUAGE 'plv8' IMMUTABLE PARALLEL SAFE STRICT
+      SET search_path FROM CURRENT
+      AS $inner$
         if (!globalThis["require"]) plv8.find_function('es_modules_init')();
         return require('pako').gzip(uncompressed, {"level":compression_level});
-      $inner$ LANGUAGE 'plv8' IMMUTABLE;
+      $inner$;
     $outer$;
 
     EXECUTE $outer$
-      CREATE OR REPLACE FUNCTION gzip(uncompressed TEXT, compression_level INTEGER) RETURNS BYTEA AS $inner$
+      CREATE OR REPLACE FUNCTION gzip(uncompressed TEXT, compression_level INTEGER) RETURNS BYTEA
+      LANGUAGE 'plv8' IMMUTABLE PARALLEL SAFE STRICT
+      SET search_path FROM CURRENT
+      AS $inner$
         if (!globalThis["require"]) plv8.find_function('es_modules_init')();
         return require('pako').gzip(uncompressed, {"level":compression_level});
-      $inner$ LANGUAGE 'plv8' IMMUTABLE;
+      $inner$;
     $outer$;
 
     EXECUTE $outer$
-      CREATE OR REPLACE FUNCTION gzip(uncompressed BYTEA) RETURNS BYTEA AS $inner$
+      CREATE OR REPLACE FUNCTION gzip(uncompressed BYTEA) RETURNS BYTEA
+      LANGUAGE 'plv8' IMMUTABLE PARALLEL SAFE STRICT
+      SET search_path FROM CURRENT
+      AS $inner$
         if (!globalThis["require"]) plv8.find_function('es_modules_init')();
         return require('pako').gzip(uncompressed);
-      $inner$ LANGUAGE 'plv8' IMMUTABLE;
+      $inner$;
     $outer$;
 
     EXECUTE $outer$
-      CREATE OR REPLACE FUNCTION gzip(uncompressed TEXT) RETURNS BYTEA AS $inner$
+      CREATE OR REPLACE FUNCTION gzip(uncompressed TEXT) RETURNS BYTEA
+      LANGUAGE 'plv8' IMMUTABLE PARALLEL SAFE STRICT
+      SET search_path FROM CURRENT
+      AS $inner$
         if (!globalThis["require"]) plv8.find_function('es_modules_init')();
         return require('pako').gzip(uncompressed);
-      $inner$ LANGUAGE 'plv8' IMMUTABLE;
+      $inner$;
     $outer$;
 
     EXECUTE $outer$
-      CREATE OR REPLACE FUNCTION gunzip(compressed BYTEA) RETURNS BYTEA AS $inner$
+      CREATE OR REPLACE FUNCTION gunzip(compressed BYTEA) RETURNS BYTEA
+      LANGUAGE 'plv8' IMMUTABLE PARALLEL SAFE STRICT
+      SET search_path FROM CURRENT
+      AS $inner$
         if (!globalThis["require"]) plv8.find_function('es_modules_init')();
         return require('pako').ungzip(compressed);
-      $inner$ LANGUAGE 'plv8' IMMUTABLE;
+      $inner$;
     $outer$;
   END IF;
 END; $sql$ LANGUAGE 'plpgsql';
