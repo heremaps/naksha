b9031c7631723c824b05e258a1d28762db63ad30
Alexander Lowey-Weber <alexander.weber@here.com>
2025-03-25 19:11:13 +0100

    Add partitionNumber(id) helper
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/Naksha.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/Naksha.kt
@@ -423,6 +423,19 @@ class Naksha private constructor() {
         internal val INT64_CLEAR_LOW16 = Int64(-65536)
 
         /**
+         * Returns the partition-number from the given feature-id.
+         *
+         * This is basically just an unsigned 16-bit integer, extracted from the lowest 16-bit of the feature-number. When there are less than 65536 partitions, the value must be divided by the number of real partitions, and the rest indexes the partition, for example for 4 partitions do `partitionNumber(featureNumber) % 4`, what will be a value between 0 and 3.
+         * @param featureId the feature-id.
+         * @return the partition-number.
+         * @see [featureNumber]
+         */
+        @JsName("featureNumberById")
+        @JsStatic
+        @JvmStatic
+        fun partitionNumber(featureId: String): Int = partitionNumber(featureNumber(featureId))
+
+        /**
          * Returns the partition-number from the given feature-number.
          *
          * This is basically just an unsigned 16-bit integer, extracted from the lowest 16-bit of the feature-number. When there are less than 65536 partitions, the value must be divided by the number of real partitions, and the rest indexes the partition, for example for 4 partitions do `partitionNumber(featureNumber) % 4`, what will be a value between 0 and 3.
