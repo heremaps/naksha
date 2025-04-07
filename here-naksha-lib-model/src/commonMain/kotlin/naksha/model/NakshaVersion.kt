8ed02ab2c9c121adcf7835914eacb0bee3189f94
Alexander Lowey-Weber <alexander.weber@here.com>
2025-04-07 14:42:37 +0200

    Increment version to beta.16
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
@@ -123,14 +123,14 @@ class NakshaVersion(
 
         const val v3_0_0_beta_8 = "3.0.0-beta.8"
 
-        // beta.9, 10, and 11 were never committed, but can be dangling in caches!
-        const val v3_0_0_beta_12 = "3.0.0-beta.12"
+        // beta.9, to 15 were never committed, but can be dangling in caches, they were used for performance tests!
+        const val v3_0_0_beta_16 = "3.0.0-beta.16"
 
         /**
          * The current version as string to constant usage cases.
          * @since 3.0
          */
-        const val CURRENT = v3_0_0_beta_12
+        const val CURRENT = v3_0_0_beta_16
 
         /**
          * The current version of the Naksha library.
