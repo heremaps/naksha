225ee07815b9c323d1236cca695acb7a10da08d3
Alexander Lowey-Weber <alexander.weber@here.com>
2025-04-30 13:05:44 +0200

    Upgrade version to 3.0.0-beta.22
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
@@ -89,7 +89,7 @@ class NakshaVersion(
          * The current version as string to constant usage cases.
          * @since 3.0
          */
-        const val CURRENT = "3.0.0-beta.21"
+        const val CURRENT = "3.0.0-beta.22"
         // WARNING: Do not update this property manually, it is automatically modified when building!
         //          Edit version only in `gradle.properties` file, which is used as well to create artifacts!
 
