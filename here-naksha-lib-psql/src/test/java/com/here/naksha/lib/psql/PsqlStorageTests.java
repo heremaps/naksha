998e6f91a9f46bd1debca4b852af8785a112d965
Hiren Patel <hiren.patel@here.com>
2025-05-14 18:46:27 +0530

    Test corrected

--- here-naksha-lib-psql/src/test/java/com/here/naksha/lib/psql/PsqlStorageTests.java
+++ here-naksha-lib-psql/src/test/java/com/here/naksha/lib/psql/PsqlStorageTests.java
@@ -1249,7 +1249,7 @@ public class PsqlStorageTests extends PsqlTests {
     expect.accept(readFeatures);
 
     // when - search by element inside an array
-    POp jsonSearch5 = POp.contains(new NonIndexedPRef("properties", "references", "0", "id"), "urn:here::here:Topology:106003684");
+    POp jsonSearch5 = POp.eq(new NonIndexedPRef("properties", "references", "0", "id"), "urn:here::here:Topology:106003684");
     readFeatures.setPropertyOp(jsonSearch5);
     // then
     expect.accept(readFeatures);
