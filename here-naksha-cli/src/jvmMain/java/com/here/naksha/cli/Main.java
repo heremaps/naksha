350b26612b55161db64ab08b346171e7e1ee5095
Bartosz Czyż <czyzbartosz3@gmail.com>
2025-07-04 11:25:45 +0200

    feat: add picocli demo
    Signed-off-by: Bartosz Czyż <czyzbartosz3@gmail.com>

--- here-naksha-cli/src/jvmMain/java/com/here/naksha/cli/Main.java
+++ here-naksha-cli/src/jvmMain/java/com/here/naksha/cli/Main.java
@@ -1,11 +1,22 @@
 package com.here.naksha.cli;
 
+import picocli.CommandLine;
+
+@CommandLine.Command(name="echo", description = "print provided string")
+class Echo implements Runnable {
+
+    @CommandLine.Parameters(index = "0", description = "string to print")
+    private String toEcho;
+
+    @Override
+    public void run() {
+        System.out.println(toEcho);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        new CommandLine(new Echo()).execute(args);
+        System.exit(0);
     }
 }
