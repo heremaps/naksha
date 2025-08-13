05417eaf2a5089707a2e22094f5fa23058a3cc90
Alexander Lowey-Weber <alexander.weber@here.com>
2025-08-13 16:46:23 +0200

    Add GuidList initializing constructor
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/GuidList.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/GuidList.kt
@@ -6,6 +6,7 @@ import naksha.base.ListProxy
 import naksha.base.Platform.Platform_C.forKClass
 import naksha.base.PlatformType
 import kotlin.js.JsExport
+import kotlin.js.JsName
 import kotlin.js.JsStatic
 import kotlin.jvm.JvmField
 
@@ -15,7 +16,19 @@ import kotlin.jvm.JvmField
  * **Warning**: A [Guid] is not serializable, and it is not possible to create it without parameters.
  */
 @JsExport
-class GuidList : ListProxy<Guid>(Guid.TYPE) {
+class GuidList() : ListProxy<Guid>(Guid.TYPE) {
+    /**
+     * Create an initialized list of [Guid].
+     * @param items the [Guid] to initialize the list with.
+     */
+    @JsName("GuidListOf")
+    constructor(vararg items: Guid?) : this() {
+        setCapacity(items.size)
+        for (item in items) {
+            add(item)
+        }
+    }
+
     companion object GuidList_C {
         /**
          * The [PlatformType] of [GuidList].
