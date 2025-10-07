5e1664a7ab9eb99c97609ff27ba32c3964516071
Marcin-Here <ext-marcin.grabowski@here.com>
2025-10-07 11:00:43 +0200

    CASL-946: HttpStorage exception fix migrated to v3 (#519)
    Signed-off-by: Marcin-Here <ext-marcin.grabowski@here.com>

--- here-naksha-storage-http/src/jvmTest/java/com/here/naksha/storage/http/HttpStoragePropertiesTest.java
+++ here-naksha-storage-http/src/jvmTest/java/com/here/naksha/storage/http/HttpStoragePropertiesTest.java
@@ -14,6 +14,7 @@ class HttpStoragePropertiesTest {
         // Then: the getters should return the predefined default values
         assertEquals(HttpStorageProperties.DEF_CONNECTION_TIMEOUT_SEC, properties.getConnectTimeout());
         assertEquals(HttpStorageProperties.DEF_SOCKET_TIMEOUT_SEC, properties.getSocketTimeout());
+        assertEquals(HttpStorageProperties.DEF_MAX_RETRIES, properties.getMaxRetries());
         assertEquals(HttpStorageProperties.DEFAULT_HEADERS, properties.getHeaders());
     }
 
@@ -24,15 +25,18 @@ class HttpStoragePropertiesTest {
         final String testUrl = "https://example.com/test";
         final int testConnectTimeout = 15;
         final int testSocketTimeout = 45;
+        final int testMaxRetries = 5;
 
         // When: we set all properties using the public setters
         properties.setUrl(testUrl);
         properties.setConnectTimeout(testConnectTimeout);
         properties.setSocketTimeout(testSocketTimeout);
+        properties.setMaxRetries(testMaxRetries);
 
         // Then: the getters should return the exact values that were set
         assertEquals(testUrl, properties.getUrl());
         assertEquals(testConnectTimeout, properties.getConnectTimeout());
         assertEquals(testSocketTimeout, properties.getSocketTimeout());
+        assertEquals(testMaxRetries, properties.getMaxRetries());
     }
 }
