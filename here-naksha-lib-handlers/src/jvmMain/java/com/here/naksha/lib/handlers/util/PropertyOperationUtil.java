c41d3a9306ae0583a7f99b08ad3a7c075a7c9398
Bartosz Czyż <czyzbartosz3@gmail.com>
2025-11-12 14:09:57 +0100

    refactor: PropertyOperationUtil
    Signed-off-by: Bartosz Czyż <czyzbartosz3@gmail.com>

--- here-naksha-lib-handlers/src/jvmMain/java/com/here/naksha/lib/handlers/util/PropertyOperationUtil.java
+++ here-naksha-lib-handlers/src/jvmMain/java/com/here/naksha/lib/handlers/util/PropertyOperationUtil.java
@@ -19,75 +19,139 @@
 package com.here.naksha.lib.handlers.util;
 
 import com.here.naksha.lib.core.lambdas.F1;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 import naksha.model.request.RequestQuery;
-import naksha.model.request.query.IPropertyQuery;
-import naksha.model.request.query.PAnd;
-import naksha.model.request.query.PFalse;
-import naksha.model.request.query.PNot;
-import naksha.model.request.query.POr;
-import naksha.model.request.query.PQuery;
-import naksha.model.request.query.PTrue;
+import naksha.model.request.query.*;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class PropertyOperationUtil {
+import java.util.*;
 
-  private PropertyOperationUtil() {
-  }
+public final class PropertyOperationUtil {
+    private static final IPropertyQuery AND_NEUTRAL = PTrue.INSTANCE;
+    private static final IPropertyQuery OR_NEUTRAL = PFalse.INSTANCE;
 
-  public static Set<PQuery> disablePQueriesInRequest(@NotNull RequestQuery requestQuery, @NotNull F1<Boolean, PQuery> shouldDisable) {
-    IPropertyQuery rootPropertyQuery = requestQuery.getProperties();
-    if (rootPropertyQuery != null) {
-      // if there is only single PQuery in the whole request, disable without tree traversal by simply removing it (set to null)
-      if (rootPropertyQuery instanceof PQuery rootPQuery && shouldDisable.call(rootPQuery)) {
-        requestQuery.setProperties(null);
-        return Set.of(rootPQuery);
-      } else {
-        // if there is a tree (not a PQuery) under `requestQuery.properties` - traverse the tree and logically disable matching pQuery
-        HashSet<PQuery> disabledProperties = new HashSet<>();
-        disablePropertyInPropertyQueryTree(rootPropertyQuery, null, shouldDisable, disabledProperties);
-        return disabledProperties;
-      }
+    private PropertyOperationUtil() {
     }
-    // root property query is null -> no disabled property queries -> empty set
-    return Collections.emptySet();
-  }
 
-  /**
-   * @param current            Currently traversed node
-   * @param parent             Parent containing current node (can be null for first iteration, should be checked on call-site)
-   * @param removalCondition   If evaluates to true, it effectively disables the check by replacing it with `true-ish` query
-   * @param disabledProperties Set of so-far disabled property queries
-   */
-  // TODO CASL-1123: this can be improved - we could inline "always true" statement such as AND(PTrue, PTrue) or OR(PTrue, PFalse)
-  // in such cases we can simply remove the node - in edge cases, we could end up without IPropertyQuery at all (if all gets resolved)
-  private static void disablePropertyInPropertyQueryTree(
-      @NotNull IPropertyQuery current, @Nullable IPropertyQuery parent, F1<Boolean, PQuery> removalCondition, Set<PQuery> disabledProperties
-  ) {
-    switch (current) {
-      case PAnd pAnd -> pAnd.forEach(andChild -> disablePropertyInPropertyQueryTree(andChild, pAnd, removalCondition, disabledProperties));
-      case POr pOr -> pOr.forEach(orChild -> disablePropertyInPropertyQueryTree(orChild, pOr, removalCondition, disabledProperties));
-      case PNot pNot -> disablePropertyInPropertyQueryTree(pNot.getQuery(), pNot, removalCondition, disabledProperties);
-      case PQuery currentyPQuery when removalCondition.call(currentyPQuery) -> {
-        if (parent instanceof PAnd pAndParent) {
-          pAndParent.remove(current);
-          pAndParent.add(PTrue.INSTANCE);
-          disabledProperties.add(currentyPQuery);
-        } else if (parent instanceof POr pOrParent) {
-          pOrParent.remove(current);
-          pOrParent.add(PTrue.INSTANCE);
-          disabledProperties.add(currentyPQuery);
-        } else if (parent instanceof PNot pNotParent) {
-          pNotParent.setQuery(PFalse.INSTANCE);
-          disabledProperties.add(currentyPQuery);
+    public static Set<PQuery> disablePQueriesInRequest(@NotNull RequestQuery requestQuery, @NotNull F1<Boolean, PQuery> shouldDisable) {
+        IPropertyQuery rootPropertyQuery = requestQuery.getProperties();
+        if (rootPropertyQuery != null) {
+            HashSet<PQuery> disabledProperties = new HashSet<>();
+            IPropertyQuery newRootPropertyQuery = disablePropertyInPropertyQueryTree(
+                    rootPropertyQuery, shouldDisable, disabledProperties, PTrue.INSTANCE
+            )
+                    .flatMap(newRoot -> {
+                        if (newRoot == PTrue.INSTANCE) {
+                            return Optional.empty();
+                        }
+                        return Optional.of(newRoot);
+                    }).orElse(null);
+            requestQuery.setProperties(newRootPropertyQuery);
+            return disabledProperties;
         }
-      }
-      default -> {
-        // unhandled type / not matching pQuery => stop traversal without failing
-      }
+        // root property query is null -> no disabled property queries -> empty set
+        return Collections.emptySet();
     }
-  }
+
+    /**
+     * @param current            Currently traversed node
+     * @param removalCondition   If evaluates to true, it effectively disables the check by replacing it with `true-ish` query
+     * @param disabledProperties Set of so-far disabled property queries
+     * @param parentsNeutral     The parent's neutral value
+     */
+    private static Optional<IPropertyQuery> disablePropertyInPropertyQueryTree(
+            @NotNull IPropertyQuery current,
+            @NotNull F1<Boolean, PQuery> removalCondition,
+            @NotNull Set<PQuery> disabledProperties,
+            @NotNull IPropertyQuery parentsNeutral
+    ) {
+        switch (current) {
+            case PAnd pAnd -> {
+                if (pAnd.stream().allMatch(pq -> pq == AND_NEUTRAL)) {
+                    return Optional.of(AND_NEUTRAL);
+                }
+                List<IPropertyQuery> newChildren = disablePropertyInChildrenQueryTree(
+                        pAnd,
+                        removalCondition,
+                        disabledProperties,
+                        AND_NEUTRAL
+                ).stream()
+                        .filter(pq -> pq != AND_NEUTRAL)
+                        .toList();
+                pAnd = new PAnd();
+                pAnd.addAll(newChildren);
+                boolean isAlwaysFalse = pAnd.stream().anyMatch(q -> q == PFalse.INSTANCE);
+                if (isAlwaysFalse) {
+                    return Optional.of(PFalse.INSTANCE);
+                }
+                return handleCompoundQuery(pAnd, parentsNeutral);
+            }
+            case POr pOr -> {
+                if (pOr.stream().allMatch(pq -> pq == OR_NEUTRAL)) {
+                    return Optional.of(OR_NEUTRAL);
+                }
+                List<IPropertyQuery> newChildren = disablePropertyInChildrenQueryTree(
+                        pOr,
+                        removalCondition,
+                        disabledProperties,
+                        OR_NEUTRAL
+                ).stream()
+                        .filter(pq -> pq != OR_NEUTRAL)
+                        .toList();
+                pOr = new POr();
+                pOr.addAll(newChildren);
+                boolean isAlwaysTrue = pOr.stream().anyMatch(q -> q == PTrue.INSTANCE);
+                if (isAlwaysTrue) {
+                    return Optional.of(PTrue.INSTANCE);
+                }
+                return handleCompoundQuery(pOr, parentsNeutral);
+            }
+            case PNot pNot -> {
+                // The pNot neutral is the negated parent's neutral
+                IPropertyQuery notNeutral = parentsNeutral == PFalse.INSTANCE ? PTrue.INSTANCE : PFalse.INSTANCE;
+                return disablePropertyInPropertyQueryTree(
+                        pNot.getQuery(), removalCondition, disabledProperties, notNeutral
+                ).flatMap(pq -> {
+                    pNot.setQuery(pq);
+                    return Optional.of(pNot);
+                });
+            }
+            case PQuery currentPQuery when removalCondition.call(currentPQuery) -> {
+                disabledProperties.add(currentPQuery);
+                return Optional.empty();
+            }
+            default -> {
+                // unhandled type / not matching pQuery => stop traversal without failing
+                return Optional.of(current);
+            }
+        }
+    }
+
+    private static <T extends List<IPropertyQuery> & IPropertyQuery> Optional<IPropertyQuery> handleCompoundQuery(
+            T compoundQuery,
+            IPropertyQuery parentsNeutral
+    ) {
+        // all the children were removed so should be neutral to parent
+        if (compoundQuery.isEmpty()) {
+            return Optional.of(parentsNeutral);
+        }
+        if (compoundQuery.size() == 1) {
+            return Optional.of(compoundQuery.getFirst());
+        }
+        return Optional.of(compoundQuery);
+    }
+
+    private static <T extends List<IPropertyQuery> & IPropertyQuery> List<IPropertyQuery> disablePropertyInChildrenQueryTree(
+            T compoundQuery,
+            F1<Boolean, PQuery> removalCondition,
+            Set<PQuery> disabledProperties,
+            IPropertyQuery parentsNeutral
+    ) {
+        return compoundQuery.stream()
+                .map(child -> disablePropertyInPropertyQueryTree(
+                        child, removalCondition, disabledProperties, parentsNeutral
+                ))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .toList();
+    }
 }
--- here-naksha-lib-handlers/src/jvmTest/java/com/here/naksha/lib/handlers/util/PropertyOperationUtilTest.java
+++ here-naksha-lib-handlers/src/jvmTest/java/com/here/naksha/lib/handlers/util/PropertyOperationUtilTest.java
@@ -1,127 +1,247 @@
 package com.here.naksha.lib.handlers.util;
 
-import static com.here.naksha.lib.handlers.util.PropertyOperationUtil.disablePQueriesInRequest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.here.naksha.lib.core.lambdas.F1;
-import java.util.List;
-import java.util.Set;
 import naksha.base.StringList;
 import naksha.model.request.RequestQuery;
-import naksha.model.request.query.DoubleOp;
-import naksha.model.request.query.IPropertyQuery;
-import naksha.model.request.query.PAnd;
-import naksha.model.request.query.PFalse;
-import naksha.model.request.query.PNot;
-import naksha.model.request.query.POr;
-import naksha.model.request.query.PQuery;
-import naksha.model.request.query.PTrue;
-import naksha.model.request.query.Property;
-import naksha.model.request.query.StringOp;
+import naksha.model.request.query.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Set;
+
+import static com.here.naksha.lib.handlers.util.PropertyOperationUtil.disablePQueriesInRequest;
+import static org.junit.jupiter.api.Assertions.*;
+
 class PropertyOperationUtilTest {
+    private final F1<Boolean, PQuery> dummyShouldDisable = pq -> false;
 
-  @Test
-  void shouldDisableSingularPQueryInRequest() {
-    // Given: request query with POp
-    PQuery pQuery = new PQuery(
-        new Property("nested", "object"),
-        StringOp.EQUALS,
-        "text_value"
-    );
-    RequestQuery query = new RequestQuery();
-    query.setProperties(pQuery);
+    @Test
+    void shouldReturnEmptySetWhenPropertiesAbsent() {
+        // Given
+        RequestQuery query = new RequestQuery();
 
-    // When
-    Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, pQueryMatchesPath("nested", "object"));
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, dummyShouldDisable);
 
-    // Then
-    assertEquals(1, disabledPQueries.size());
-    assertEquals(pQuery, disabledPQueries.iterator().next());
-  }
+        // Then
+        assertEquals(0, disabledPQueries.size());
+        assertNull(query.getProperties());
+    }
 
-  @Test
-  void shouldComposedPQueryInRequest() {
-    // Given: request with dummy query with POp:
-    // - query all speed limits of 60 AND "car allowed" signs
-    // - "car allowed signs": signs with type `car_allowed` or value that is NOT `can_not_allowed`
-    PQuery typeIsSpeedLimit = new PQuery(new Property("sign", "type"), StringOp.EQUALS, "speed_limit");
-    PQuery valueIs60 = new PQuery(new Property("sign", "value"), DoubleOp.EQ, 60.0);
-    PQuery typeIsCarAllowed = new PQuery(new Property("sign", "type"), StringOp.EQUALS, "car_allowed");
-    PQuery valueIsCarNotAllowed = new PQuery(new Property("sign", "value"), StringOp.EQUALS, "car_not_allowed");
-    IPropertyQuery originalPropertyQuery = new POr(
-        new PAnd(typeIsSpeedLimit, valueIs60),
-        new POr(typeIsCarAllowed, new PNot(valueIsCarNotAllowed))
-    );
-    RequestQuery query = new RequestQuery();
-    query.setProperties(originalPropertyQuery);
+    @Test
+    void shouldReduceTrivialAnd() {
+        // Given
+        RequestQuery query = new RequestQuery();
+        IPropertyQuery propertyQuery = new PAnd(
+                PTrue.INSTANCE,
+                PTrue.INSTANCE
+        );
+        query.setProperties(propertyQuery);
 
-    // When: disabling all queries related to `sign.value`
-    Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, pQueryMatchesPath("sign", "value"));
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, dummyShouldDisable);
 
-    // Then: disabled subqueries are about value
-    assertEquals(2, disabledPQueries.size());
-    assertTrue(disabledPQueries.containsAll(List.of(valueIs60, valueIsCarNotAllowed)));
+        // Then
+        assertEquals(0, disabledPQueries.size());
+        assertNull(query.getProperties());
+    }
 
-    // And: original request was correctly mutated
-    IPropertyQuery mutatedPropertyQuery = query.getProperties();
-    assertSame(originalPropertyQuery, mutatedPropertyQuery,
-        "Property query should be mutated in place - mutated and original query must refer to the same instance");
-    // root is OR with 2 child nodes
-    assertInstanceOf(POr.class, mutatedPropertyQuery);
-    POr root = (POr) mutatedPropertyQuery;
-    assertEquals(2, root.size());
-    // first child is AND with 2 child nodes - including PTrue made from `valueIs60`
-    assertInstanceOf(PAnd.class, root.get(0));
-    PAnd andUnderRoot = (PAnd) root.get(0);
-    assertEquals(2, andUnderRoot.size());
-    assertEquals(typeIsSpeedLimit, andUnderRoot.get(0));
-    assertEquals(PTrue.INSTANCE, andUnderRoot.get(1));
-    // second child is OR with 2 child nodes - including mutated PNot with PFalse made from `valueIsCarNotAllowed`:
-    assertInstanceOf(POr.class, root.get(1));
-    POr orUnderRoot = (POr) root.get(1);
-    assertEquals(2, orUnderRoot.size());
-    assertEquals(typeIsCarAllowed, orUnderRoot.get(0));
-    assertInstanceOf(PNot.class, orUnderRoot.get(1));
-    PNot nestedPNot = (PNot) orUnderRoot.get(1);
-    assertEquals(PFalse.INSTANCE, nestedPNot.getQuery());
-  }
+    @Test
+    void shouldReduceAlwaysFalseAnd() {
+        // Given
+        RequestQuery query = new RequestQuery();
+        IPropertyQuery propertyQuery = new PAnd(
+                PTrue.INSTANCE,
+                PFalse.INSTANCE
+        );
+        query.setProperties(propertyQuery);
 
-  @Test
-  void shouldDisableSimpleOr(){
-    // Given
-    POr root = new POr();
-    root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "a"));
-    root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "b"));
-    RequestQuery query = new RequestQuery();
-    query.setProperties(root);
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, dummyShouldDisable);
 
-    // When
-    PropertyOperationUtil.disablePQueriesInRequest(query, pQueryMatchesPath("foo", "bar"));
+        // Then
+        assertEquals(0, disabledPQueries.size());
+        assertEquals(PFalse.INSTANCE, query.getProperties());
+    }
 
-    // Then
-    POr mutatedPropertyQuery = (POr) query.getProperties();
-    assertEquals(2, mutatedPropertyQuery.size());
-    assertInstanceOf(PTrue.class, mutatedPropertyQuery.get(0));
-    assertInstanceOf(PTrue.class, mutatedPropertyQuery.get(1));
-  }
+    @Test
+    void shouldReduceTrivialOr() {
+        // Given
+        RequestQuery query = new RequestQuery();
+        IPropertyQuery propertyQuery = new POr(
+                PFalse.INSTANCE,
+                PFalse.INSTANCE
+        );
+        query.setProperties(propertyQuery);
 
-  private F1<Boolean, PQuery> pQueryMatchesPath(String... expectedPath) {
-    return pQuery -> {
-      StringList queryPath = pQuery.getProperty().getPath();
-      if (expectedPath.length != queryPath.size()) {
-        return false;
-      }
-      for (int i = 0; i < expectedPath.length; i++) {
-        if (!expectedPath[i].equals(queryPath.get(i))) {
-          return false;
-        }
-      }
-      return true;
-    };
-  }
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, dummyShouldDisable);
+
+        // Then
+        assertEquals(0, disabledPQueries.size());
+        assertEquals(PFalse.INSTANCE, query.getProperties());
+    }
+
+    @Test
+    void shouldReduceAlwaysTrueOr() {
+        // Given
+        RequestQuery query = new RequestQuery();
+        IPropertyQuery propertyQuery = new POr(
+                PFalse.INSTANCE,
+                PTrue.INSTANCE
+        );
+        query.setProperties(propertyQuery);
+
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, dummyShouldDisable);
+
+        // Then
+        assertEquals(0, disabledPQueries.size());
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldDisableSingularPQueryInRequest() {
+        // Given: request query with POp
+        PQuery pQuery = new PQuery(
+                new Property("nested", "object"),
+                StringOp.EQUALS,
+                "text_value"
+        );
+        RequestQuery query = new RequestQuery();
+        query.setProperties(pQuery);
+
+        // When
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, pQueryMatchesPath("nested", "object"));
+
+        // Then
+        assertEquals(1, disabledPQueries.size());
+        assertEquals(pQuery, disabledPQueries.iterator().next());
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldDisablePOrWhenAllChildrenDisabled() {
+        // Given
+        PQuery valueIs60 = new PQuery(new Property("sign", "value"), DoubleOp.EQ, 60.0);
+        PQuery valueIsCarNotAllowed = new PQuery(new Property("sign", "value"), StringOp.EQUALS, "car_not_allowed");
+        IPropertyQuery originalPropertyQuery = new POr(
+                valueIs60,
+                valueIsCarNotAllowed
+        );
+        RequestQuery query = new RequestQuery();
+        query.setProperties(originalPropertyQuery);
+
+        // When: disabling all queries related to `sign.value`
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, pQueryMatchesPath("sign", "value"));
+
+        // Then: disabled subqueries are about value
+        assertEquals(2, disabledPQueries.size());
+        assertTrue(disabledPQueries.containsAll(List.of(valueIs60, valueIsCarNotAllowed)));
+
+        // And: original request was correctly mutated
+        IPropertyQuery mutatedPropertyQuery = query.getProperties();
+        assertNull(mutatedPropertyQuery);
+    }
+
+    @Test
+    void shouldComposedPQueryInRequest() {
+        // Given: request with dummy query with POp:
+        // - query all speed limits of 60 AND "car allowed" signs
+        // - "car allowed signs": signs with type `car_allowed` or value that is NOT `can_not_allowed`
+        IPropertyQuery typeIsSpeedLimit = new PNot(new PQuery(new Property("sign", "type"), StringOp.EQUALS, "speed_limit"));
+        PQuery valueIs60 = new PQuery(new Property("sign", "value"), DoubleOp.EQ, 60.0);
+        PQuery typeIsCarAllowed = new PQuery(new Property("sign", "type"), StringOp.EQUALS, "car_allowed");
+        PQuery valueIsCarNotAllowed = new PQuery(new Property("sign", "value"), StringOp.EQUALS, "car_not_allowed");
+        IPropertyQuery originalPropertyQuery = new POr(
+                new PAnd(new PNot(typeIsSpeedLimit), valueIs60),
+                new POr(typeIsCarAllowed, new PNot(valueIsCarNotAllowed))
+        );
+        RequestQuery query = new RequestQuery();
+        query.setProperties(originalPropertyQuery);
+
+        // When: disabling all queries related to `sign.value`
+        Set<PQuery> disabledPQueries = disablePQueriesInRequest(query, pQueryMatchesPath("sign", "value"));
+
+        // Then: disabled subqueries are about value
+        assertEquals(2, disabledPQueries.size());
+        assertTrue(disabledPQueries.containsAll(List.of(valueIs60, valueIsCarNotAllowed)));
+
+        // And: request was correctly mutated
+        IPropertyQuery newPropertyQuery = query.getProperties();
+        assertNotNull(newPropertyQuery);
+        // root is OR with 2 child nodes
+        assertInstanceOf(POr.class, newPropertyQuery);
+        POr root = (POr) newPropertyQuery;
+        assertEquals(2, root.size());
+        // first child is PNot
+        PNot firstChild = assertInstanceOf(PNot.class, root.get(0));
+        // second child is typeIsCarAllowed
+        assertEquals(typeIsCarAllowed, root.get(1));
+        // and typeIsSpeedLimit in firstChild
+        assertNotNull(firstChild);
+        assertEquals(typeIsSpeedLimit, firstChild.getQuery());
+    }
+
+    @Test
+    void shouldDisableSimpleOr() {
+        // Given
+        POr root = new POr();
+        root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "a"));
+        root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "b"));
+        RequestQuery query = new RequestQuery();
+        query.setProperties(root);
+
+        // When
+        PropertyOperationUtil.disablePQueriesInRequest(query, pQueryMatchesPath("foo", "bar"));
+
+        // Then
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldDisableSimpleAnd() {
+        // Given
+        PAnd root = new PAnd();
+        root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "a"));
+        root.add(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "b"));
+        RequestQuery query = new RequestQuery();
+        query.setProperties(root);
+
+        // When
+        PropertyOperationUtil.disablePQueriesInRequest(query, pQueryMatchesPath("foo", "bar"));
+
+        // Then
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldDisableSimpleNot() {
+        // Given
+        PNot root = new PNot();
+        root.setQuery(new PQuery(new Property("foo", "bar"), StringOp.EQUALS, "a"));
+        RequestQuery query = new RequestQuery();
+        query.setProperties(root);
+
+        // When
+        PropertyOperationUtil.disablePQueriesInRequest(query, pQueryMatchesPath("foo", "bar"));
+
+        // Then
+        assertNull(query.getProperties());
+    }
+
+    private F1<Boolean, PQuery> pQueryMatchesPath(String... expectedPath) {
+        return pQuery -> {
+            StringList queryPath = pQuery.getProperty().getPath();
+            if (expectedPath.length != queryPath.size()) {
+                return false;
+            }
+            for (int i = 0; i < expectedPath.length; i++) {
+                if (!expectedPath[i].equals(queryPath.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
 }
