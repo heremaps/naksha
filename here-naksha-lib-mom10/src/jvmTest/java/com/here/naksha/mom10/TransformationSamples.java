297ba563067d76838780e46a53fc8af6085d03f7
Jakub Amanowicz <jakub.amanowicz@here.com>
2025-11-21 14:09:12 +0100

    V3 casl 1466 v3 mom 10 (#538)
    * cherry picked mom10 from v2
    
    Signed-off-by: Jakub Amanowicz <jakub.amanowicz@here.com>
    
    * CASL-1466 migrated mom10 module to v3
    
    Signed-off-by: Jakub Amanowicz <jakub.amanowicz@here.com>
    
    * CASL-1466 review fixes
    
    Signed-off-by: Jakub Amanowicz <jakub.amanowicz@here.com>
    
    ---------
    
    Signed-off-by: Jakub Amanowicz <jakub.amanowicz@here.com>

---
+++ here-naksha-lib-mom10/src/jvmTest/java/com/here/naksha/mom10/TransformationSamples.java
@@ -0,0 +1,97 @@
+package com.here.naksha.mom10;
+
+import static java.nio.file.Files.readAllBytes;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import naksha.base.JvmBoxingUtil;
+import naksha.base.Platform;
+import naksha.model.objects.NakshaFeature;
+
+public class TransformationSamples {
+
+  private TransformationSamples() {
+  }
+
+  public record TransformationSample(
+      String sourceDir,
+      NakshaFeature mom10,
+      NakshaFeature nakshaInternal
+  ) {
+
+    @Override
+    public String toString() {
+      return "Samples from: '" + sourceDir + "'";
+    }
+  }
+
+  public static Stream<TransformationSample> streamSamples() {
+    return getSamples().stream();
+  }
+
+  private static List<TransformationSample> getSamples() {
+    // we copy loaded samples to avoid mutations
+    List<TransformationSample> samples = new ArrayList<>(SamplesLoader.LOADED_SAMPLES.size());
+    for (TransformationSample base : SamplesLoader.LOADED_SAMPLES) {
+      samples.add(copy(base));
+    }
+    return samples;
+  }
+
+  private static TransformationSample copy(TransformationSample base) {
+    return new TransformationSample(
+        base.sourceDir,
+        base.mom10.copy(true),
+        base.nakshaInternal.copy(true)
+    );
+  }
+
+  private static class SamplesLoader {
+
+    private static final String TEST_DIR = "/transformation_samples";
+    private static final String MOM_10_JSON = "mom_10.json";
+    private static final String NAKSHA_INTERNAL_JSON = "naksha_internal.json";
+
+    // initialization on demand
+    private static final List<TransformationSample> LOADED_SAMPLES;
+
+    static {
+      List<Path> dirs = samplesDirs();
+      LOADED_SAMPLES = new ArrayList<>(dirs.size());
+      for (Path dir : dirs) {
+        LOADED_SAMPLES.add(loadSampleFrom(dir));
+      }
+    }
+
+    private static List<Path> samplesDirs() {
+      try {
+        Path testDir = Paths.get(TransformationSamples.class.getResource(TEST_DIR).toURI());
+        try (Stream<Path> subdirs = Files.list(testDir)) {
+          return subdirs.filter(Files::isDirectory).toList(); // not reusing underlying stream as it will get closed
+        }
+      } catch (URISyntaxException | IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    private static TransformationSample loadSampleFrom(Path dir) {
+      try {
+        byte[] rawBefore = readAllBytes(dir.resolve(MOM_10_JSON));
+        byte[] rawAfter = readAllBytes(dir.resolve(NAKSHA_INTERNAL_JSON));
+        return new TransformationSample(
+            dir.getFileName().toString(),
+            JvmBoxingUtil.box(Platform.fromJSON(rawBefore), NakshaFeature.class),
+            JvmBoxingUtil.box(Platform.fromJSON(rawAfter), NakshaFeature.class)
+        );
+      } catch (IOException e) {
+        throw new RuntimeException("Unable to load sample from: " + dir, e);
+      }
+    }
+  }
+}
---
+++ here-naksha-lib-mom10/src/jvmTest/java/com/here/naksha/mom10/transform/Mom10TransformationTest.java
@@ -0,0 +1,34 @@
+package com.here.naksha.mom10.transform;
+
+import static com.here.naksha.mom10.util.FeaturesAssertionUtil.assertFeaturesEqual;
+
+import com.here.naksha.mom10.Mom10Transformation;
+import com.here.naksha.mom10.TransformationSamples.TransformationSample;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class Mom10TransformationTest {
+
+  @ParameterizedTest
+  @MethodSource("com.here.naksha.mom10.TransformationSamples#streamSamples")
+  void shouldPopulatePreMom10Namespaces(TransformationSample versionedFeatures) {
+    // When
+    Mom10Transformation.populatePreMom10Namespaces(versionedFeatures.mom10());
+
+    // Then:
+    assertFeaturesEqual(
+        versionedFeatures.nakshaInternal(), // expected - untouched
+        versionedFeatures.mom10() // actual - transformed
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("com.here.naksha.mom10.TransformationSamples#streamSamples")
+  void shouldDropPreMom10Namespaces(TransformationSample versionedFeatures) {
+    // When
+    Mom10Transformation.dropPreMom10Namespaces(versionedFeatures.nakshaInternal());
+
+    // Then:
+    assertFeaturesEqual(versionedFeatures.mom10(), versionedFeatures.nakshaInternal());
+  }
+}
