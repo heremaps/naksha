eb4598285eafdfd710d785fccbf0cdaaf2b8b30c
Bartosz Czyż <czyzbartosz3@gmail.com>
2026-01-05 17:03:36 +0100

    fix&tests

--- here-naksha-lib-handlers/src/jvmTest/java/com/here/naksha/lib/handlers/util/PropertyOperationUtilTest.java
+++ here-naksha-lib-handlers/src/jvmTest/java/com/here/naksha/lib/handlers/util/PropertyOperationUtilTest.java
@@ -1,6 +1,7 @@
 package com.here.naksha.lib.handlers.util;
 
 import com.here.naksha.lib.core.lambdas.F1;
+import java.util.stream.Stream;
 import naksha.base.StringList;
 import naksha.model.request.RequestQuery;
 import naksha.model.request.query.*;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Set;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static com.here.naksha.lib.handlers.util.PropertyOperationUtil.disablePQueriesInRequest;
 import static org.junit.jupiter.api.Assertions.*;
@@ -149,7 +152,7 @@ class PropertyOperationUtilTest {
         // Given: request with dummy query with POp:
         // - query all speed limits of 60 AND "car allowed" signs
         // - "car allowed signs": signs with type `car_allowed` or value that is NOT `can_not_allowed`
-        IPropertyQuery typeIsSpeedLimit = new PNot(new PQuery(new Property("sign", "type"), StringOp.EQUALS, "speed_limit"));
+        PQuery typeIsSpeedLimit = new PQuery(new Property("sign", "type"), StringOp.EQUALS, "speed_limit");
         PQuery valueIs60 = new PQuery(new Property("sign", "value"), DoubleOp.EQ, 60.0);
         PQuery typeIsCarAllowed = new PQuery(new Property("sign", "type"), StringOp.EQUALS, "car_allowed");
         PQuery valueIsCarNotAllowed = new PQuery(new Property("sign", "value"), StringOp.EQUALS, "car_not_allowed");
@@ -230,6 +233,206 @@ class PropertyOperationUtilTest {
         assertNull(query.getProperties());
     }
 
+    @Test
+    void shouldRemoveDisabledChildFromAndAndKeepOther() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+        PQuery b = new PQuery(new Property("b"), StringOp.EQUALS, "2");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PAnd(a, b));
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(Set.of(a), disabled);
+        assertEquals(b, query.getProperties());
+    }
+
+    @Test
+    void shouldRemoveDisabledChildFromOrAndKeepOther() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+        PQuery b = new PQuery(new Property("b"), StringOp.EQUALS, "2");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new POr(a, b));
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(Set.of(a), disabled);
+        assertEquals(b, query.getProperties());
+    }
+
+    @Test
+    void shouldReduceAndToFalseWhenFalseRemains() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PAnd(a, PFalse.INSTANCE));
+
+        // When
+        disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(PFalse.INSTANCE, query.getProperties());
+    }
+
+    @Test
+    void shouldReduceOrToTrueWhenTrueRemains() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new POr(a, PTrue.INSTANCE));
+
+        // When
+        disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldRemoveNotWhenInnerQueryDisabled() {
+        // Given
+        PQuery inner = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+        PNot root = new PNot(inner);
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(root);
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(Set.of(inner), disabled);
+        assertNull(query.getProperties());
+    }
+
+    @Test
+    void shouldHandleNotAndWithOneDisabledChild() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+        PQuery b = new PQuery(new Property("b"), StringOp.EQUALS, "2");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PNot(new PAnd(a, b)));
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(Set.of(a), disabled);
+        IPropertyQuery result = query.getProperties();
+        assertInstanceOf(PNot.class, result);
+        assertEquals(b, ((PNot) result).getQuery());
+    }
+
+    @Test
+    void shouldCollapseNestedAndOrCorrectly() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+        PQuery b = new PQuery(new Property("b"), StringOp.EQUALS, "2");
+        PQuery c = new PQuery(new Property("c"), StringOp.EQUALS, "3");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PAnd(
+            new POr(a, b),
+            c
+        ));
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(Set.of(a), disabled);
+
+        IPropertyQuery result = query.getProperties();
+        assertInstanceOf(PAnd.class, result);
+        PAnd and = (PAnd) result;
+
+        assertEquals(2, and.size());
+        assertInstanceOf(PQuery.class, and.get(1));
+    }
+
+    @Test
+    void shouldNotDuplicateDisabledQueries() {
+        // Given
+        PQuery a = new PQuery(new Property("a"), StringOp.EQUALS, "1");
+
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PAnd(a, a));
+
+        // When
+        Set<PQuery> disabled = disablePQueriesInRequest(query, pQueryMatchesPath("a"));
+
+        // Then
+        assertEquals(1, disabled.size());
+        assertTrue(disabled.contains(a));
+    }
+
+    @ParameterizedTest
+    @MethodSource("andTruthTable")
+    void truthTable_and(IPropertyQuery left, IPropertyQuery right, IPropertyQuery expected) {
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PAnd(left, right));
+
+        disablePQueriesInRequest(query, pq -> true);
+
+        assertEquals(expected, query.getProperties());
+    }
+
+    @ParameterizedTest
+    @MethodSource("orTruthTable")
+    void truthTable_or(IPropertyQuery left, IPropertyQuery right, IPropertyQuery expected) {
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new POr(left, right));
+
+        disablePQueriesInRequest(query, pq -> true);
+
+        assertEquals(expected, query.getProperties());
+    }
+
+    @ParameterizedTest
+    @MethodSource("notTruthTable")
+    void truthTable_not(IPropertyQuery inner, IPropertyQuery expected) {
+        RequestQuery query = new RequestQuery();
+        query.setProperties(new PNot(inner));
+
+        disablePQueriesInRequest(query, pq -> true);
+
+        assertEquals(expected, query.getProperties());
+    }
+
+    private static Stream<Object[]> notTruthTable() {
+        return Stream.of(
+            new Object[]{PTrue.INSTANCE,  PFalse.INSTANCE},
+            new Object[]{PFalse.INSTANCE, null}
+        );
+    }
+
+    private static Stream<Object[]> andTruthTable() {
+        return Stream.of(
+            new Object[]{PTrue.INSTANCE,  PTrue.INSTANCE,  null},
+            new Object[]{PTrue.INSTANCE,  PFalse.INSTANCE, PFalse.INSTANCE},
+            new Object[]{PFalse.INSTANCE, PTrue.INSTANCE,  PFalse.INSTANCE},
+            new Object[]{PFalse.INSTANCE, PFalse.INSTANCE, PFalse.INSTANCE}
+        );
+    }
+
+    private static Stream<Object[]> orTruthTable() {
+        return Stream.of(
+            new Object[]{PTrue.INSTANCE,  PTrue.INSTANCE,  null},
+            new Object[]{PTrue.INSTANCE,  PFalse.INSTANCE, null},
+            new Object[]{PFalse.INSTANCE, PTrue.INSTANCE,  null},
+            new Object[]{PFalse.INSTANCE, PFalse.INSTANCE, PFalse.INSTANCE}
+        );
+    }
     private F1<Boolean, PQuery> pQueryMatchesPath(String... expectedPath) {
         return pQuery -> {
             StringList queryPath = pQuery.getProperty().getPath();
