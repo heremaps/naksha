7c226fd37ee05dd07f69f6c28f9ba1961f13842b
Alexander Lowey-Weber <alexander.weber@here.com>
2026-07-22 14:37:04 +0200

    Fix some merge issues, increased version to 3.0.0-beta.43.
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonTest/kotlin/naksha/model/TransactionNumberTest.kt
+++ here-naksha-lib-model/src/commonTest/kotlin/naksha/model/TransactionNumberTest.kt
@@ -1,15 +1,17 @@
 package naksha.model
 
+import naksha.base.Action
 import naksha.base.Int64
+import naksha.base.Version
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class TransactionNumberTest {
     @Test
     fun versionFromToString() {
-        val v1 = Version.of(2024, 5, 10, Int64(12345))
+        val v1 = Version.auto(2024, 5, 10, Int64(12345), Action.VERSION)
         val s = v1.toString()
         val parsed = Version.fromString(s)
-        assertEquals(v1.txn, parsed.txn)
+        assertEquals(v1.number, parsed.number)
     }
 }
