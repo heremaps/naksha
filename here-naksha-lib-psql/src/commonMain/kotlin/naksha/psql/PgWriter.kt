e6ca244db999f65c00f19b4c04e926afac73dd78
Phuc Mai <39376416+gunplar@users.noreply.github.com>
2026-07-30 17:15:23 +0200

    Fix so that WriteRequest execution does not mutate input NakshaFeature (#642)
    * fix so that WriteRequest execution does not mutate input NakshaFeature
    
    Signed-off-by: phmai <phuc.mai@here.com>
    
    * null handling
    
    Signed-off-by: phmai <phuc.mai@here.com>
    
    * bump v3 version
    
    Signed-off-by: phmai <phuc.mai@here.com>
    
    ---------
    
    Signed-off-by: phmai <phuc.mai@here.com>

--- here-naksha-lib-psql/src/commonMain/kotlin/naksha/psql/PgWriter.kt
+++ here-naksha-lib-psql/src/commonMain/kotlin/naksha/psql/PgWriter.kt
@@ -276,13 +276,17 @@ open class PgWriter internal constructor(
             }
             when (op) {
                 WriteOp.CREATE -> {
-                    val f: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    //TODO fix this hack (cloning feature) at the source i.e. Tuple.encodeFeature(), the intention is not to mutate the input
+                    val original: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    val f = original.copy<NakshaFeature>(true)
                     val tuple = Tuple.encodeFeature(f, pgCollection.head, session, null, Action.CREATE, pgWrite.atomic)
                     pgWrite.tuple = tuple
                     pgWrite.tupleNumber = tuple.tupleNumber
                 }
                 WriteOp.UPDATE -> {
-                    val f: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    //TODO fix this hack (cloning feature) at the source i.e. Tuple.encodeFeature(), the intention is not to mutate the input
+                    val original: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    val f = original.copy<NakshaFeature>(true)
                     val tuple = Tuple.encodeFeature(f, pgCollection.head, session, null, Action.UPDATE, pgWrite.atomic)
                     pgWrite.tuple = tuple
                     pgWrite.tupleNumber = tuple.tupleNumber
@@ -294,7 +298,9 @@ open class PgWriter internal constructor(
                     //
                     // To stay downward compatible, we therefore remove (as a hack) the UUID, so we ensure that we get a CREATE.
                     // TODO: Remove this hack and remove UPSERT completely from storage.
-                    val f: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    //TODO fix this hack (cloning feature) at the source i.e. Tuple.encodeFeature(), the intention is not to mutate the input
+                    val original: NakshaFeature = pgWrite.feature ?: throw illegalArg("The feature #${pgWrite.i} is null")
+                    val f = original.copy<NakshaFeature>(true)
                     val nakshaCollection = pgWrite.collection.head
                     val uuidMember = nakshaCollection.useMember(StandardMembers.Tn)
                     uuidMember.delete(f)
