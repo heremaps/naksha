51e5bc903c45bbcc05a5086a33593edbbc19b898
Alexander Lowey-Weber <alexander.weber@here.com>
2026-08-27 17:08:53 +0200

    Improve array handling, whenever possible use native array without converting, improve type detection.
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-psql/src/jvmMain/kotlin/naksha/psql/PsqlQuery.kt
+++ here-naksha-lib-psql/src/jvmMain/kotlin/naksha/psql/PsqlQuery.kt
@@ -1,31 +1,15 @@
 package naksha.psql
 
 import naksha.base.Int64
+import naksha.base.JvmInt64
 import naksha.base.ListProxy
 import naksha.base.MapProxy
 import naksha.base.Platform
 import naksha.base.illegalArg
-import naksha.psql.PgType.Companion.BOOLEAN
-import naksha.psql.PgType.Companion.BOOLEAN_ARRAY
-import naksha.psql.PgType.Companion.BYTE_ARRAY
-import naksha.psql.PgType.Companion.BYTE_ARRAY_ARRAY
-import naksha.psql.PgType.Companion.DOUBLE
-import naksha.psql.PgType.Companion.DOUBLE_ARRAY
-import naksha.psql.PgType.Companion.FLOAT
-import naksha.psql.PgType.Companion.FLOAT_ARRAY
-import naksha.psql.PgType.Companion.INT
-import naksha.psql.PgType.Companion.INT64
-import naksha.psql.PgType.Companion.INT64_ARRAY
-import naksha.psql.PgType.Companion.INT_ARRAY
-import naksha.psql.PgType.Companion.JSONB
-import naksha.psql.PgType.Companion.JSONB_ARRAY
-import naksha.psql.PgType.Companion.SHORT
-import naksha.psql.PgType.Companion.SHORT_ARRAY
-import naksha.psql.PgType.Companion.STRING
-import naksha.psql.PgType.Companion.STRING_ARRAY
 import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.ResultSet.*
+import kotlin.reflect.KClass
 
 /**
  * A helper to parse SQL queries and find the dollar-placeholders, replacing them with `?`, escape question-marks, and finally provide a
@@ -34,10 +18,17 @@ import java.sql.ResultSet.*
 class PsqlQuery(query: String, private val typeNames: Array<String>?) {
 
     /**
-     * We map "$1" to a list of positions (`1..n`) in the prepared statement. For example
-     * `SELECT * FROM table WHERE a = $1 OR b = $1`. In that case, we replace it with
-     * `SELECT * FROM table WHERE a = ? OR b = ?` and the map holds one entry 1 and a
-     * list of two elements 1 and 2.
+     * A map of placeholder positions (`1..n`) in the prepared statement.
+     *
+     * For example
+     * ```sql
+     * SELECT * FROM table WHERE a=$1 OR b=$1
+     * ```
+     * In that case JDBC requires:
+     * ```sql
+     * SELECT * FROM table WHERE a=? OR b=?
+     * ```
+     * This map will then contain a key `1` _(which is the dollar-index)_, assigned to an array with the JDBC indices to bind. In this case `1` and `2`. When binding the arguments the code needs to bind it to all indices. Beware that JDBC positions start at index `1` not `0` and the index array contains already the correct JDBC values, so all values will be between `1` and `n` _(inclusive)_!
      */
     val dollarToIndices: HashMap<Int, ArrayList<Int>> = HashMap()
     val sql: String
@@ -87,83 +78,129 @@ class PsqlQuery(query: String, private val typeNames: Array<String>?) {
         }
     }
 
-    private fun setArgument(stmt: PreparedStatement, arg: Any?, indices: ArrayList<Int>) {
-        var i = 0
-        while (i < indices.size) {
-            // Note: `index` starts with 1, NOT 0 !!!
-            val index = indices[i++]
-            when (arg) {
-                null -> stmt.setNull(index, 0)
-                is Boolean -> stmt.setBoolean(index, arg)
-                is Short -> stmt.setShort(index, arg)
-                is Int -> stmt.setInt(index, arg)
-                is Long -> stmt.setLong(index, arg)
-                is Int64 -> stmt.setLong(index, arg.toLong())
-                is Float -> stmt.setFloat(index, arg)
-                is Double -> stmt.setDouble(index, arg)
-                is String -> stmt.setString(index, arg)
-                is ByteArray -> stmt.setBytes(index, arg)
-                is Array<*> -> {
-                    // Note: Java array indices start at 0, NOT 1 !!!
-                    val typeNameIndex = index - 1
-                    val typeName = if (typeNames != null && typeNameIndex < typeNames.size) typeNames[typeNameIndex] else null
-                    var type = PgType.of(typeName)
-                    if (type == null) {
-                        if (arg.size == 0) throw illegalArg("Can't detect type of empty array, declared type: $typeName")
-                        var j = 0
-                        while (type == null && j < arg.size) {
-                            val testValue = arg[j++]
-                            type = when (testValue) {
-                                null -> null
-                                is Boolean -> BOOLEAN_ARRAY
-                                is Short -> SHORT_ARRAY
-                                is Int -> INT_ARRAY
-                                is Int64, is Long -> INT64_ARRAY
-                                is Float -> FLOAT_ARRAY
-                                is Double -> DOUBLE_ARRAY
-                                is String -> STRING_ARRAY
-                                is ByteArray -> BYTE_ARRAY_ARRAY
-                                else -> throw illegalArg("Auto detection of array-type failed due to unknown value for \$$index, declared type: $typeName, found type: ${testValue::class.simpleName}")
+    /**
+     * Binds the given argument in the given prepared statement.
+     *
+     * Beware that we support the natural PostgresQL syntax, which allows to refer the same value multiple times, so for example `WHERE a = $1 OR b = $1` is allowed, however, in JDBC we have to translate this into `WHERE a = ? OR b = ?` and we now need to bind both placeholders to the same value. Therefore, the argument `indices` provides the zero-based indices in the query where the corresponding question mark is located. This means, we may need to bind the same argument multiple times.
+     * @param stmt the statement.
+     * @param arg the value to bind.
+     * @param typeName the type name as specified in the prepared statement; if any is available.
+     * @param indices a list of indices to bind the argument to.
+     */
+    private fun setArgument(stmt: PreparedStatement, arg: Any?, typeName: String?, indices: ArrayList<Int>) {
+        val conn = stmt.connection
+        when (arg) {
+            // Note: `index` in JDBC starts with 1, NOT 0!
+            null -> for (index in indices) stmt.setNull(index, 0)
+            // Recursive call, simply convert the list into an array, then it's handled below.
+            is ListProxy<*> -> setArgument(stmt, arg.toArray(), typeName, indices)
+            // Wrapped primitives.
+            is Boolean -> for (index in indices) stmt.setBoolean(index, arg)
+            is Byte -> for (index in indices) stmt.setShort(index, arg.toShort())
+            is Short -> for (index in indices) stmt.setShort(index, arg)
+            is Int -> for (index in indices) stmt.setInt(index, arg)
+            is Long -> for (index in indices) stmt.setLong(index, arg)
+            is Int64 -> for (index in indices) stmt.setLong(index, arg.toLong())
+            is Float -> for (index in indices) stmt.setFloat(index, arg)
+            is Double -> for (index in indices) stmt.setDouble(index, arg)
+            is String -> for (index in indices) stmt.setString(index, arg)
+            is CharSequence -> for (index in indices) stmt.setString(index, arg.toString())
+            // Handle Primitive Arrays.
+            // https://jdbc.postgresql.org/documentation/server-prepare/#arrays
+            is ByteArray -> for (index in indices) stmt.setBytes(index, arg)
+            is BooleanArray -> for (index in indices) stmt.setObject(index, arg)
+            is ShortArray -> for (index in indices) stmt.setObject(index, arg)
+            is IntArray -> for (index in indices) stmt.setObject(index, arg)
+            is LongArray -> for (index in indices) stmt.setObject(index, arg)
+            is FloatArray -> for (index in indices) stmt.setObject(index, arg)
+            is DoubleArray -> for (index in indices) stmt.setObject(index, arg)
+            // Handle Array<*>.
+            is Array<*> -> {
+                // https://jdbc.postgresql.org/documentation/server-prepare/#arrays
+                var componentType = arg.javaClass.componentType!!
+                var array: Array<*> = arg
+                if (componentType == Any::class.java || componentType == Int64::class.java || componentType == JvmInt64::class.java) {
+                    // Special handling for `Array<Any>`:
+                    // Array of objects need to be turned either into a `java.sql.Array` or converted into a `Array<Type>`.
+                    // The reason is that the driver only accepts arrays with same content, and if the content is null,
+                    // it still needs to know the type.
+                    // We treat Int64 as Long until we switched finally!
+                    if (componentType == Int64::class.java || componentType == JvmInt64::class.java) {
+                        componentType = Long::class.javaObjectType
+                    } else for (value in arg) {
+                        if (value == null) continue
+                        componentType = value.javaClass
+                        if (componentType == Int64::class.java || componentType == JvmInt64::class.java) {
+                            componentType = Long::class.javaObjectType
+                        }
+                        break
+                    }
+                    if (componentType == Any::class.java) {
+                        // Try to detect the component type from what the client provided in the prepared statement.
+                        if (typeName != null) {
+                            val pgType = PgType.of(typeName)
+                            if (pgType != null) {
+                                val klass: KClass<*>? = if (pgType.isArray) pgType.componentType?.klass else pgType.klass
+                                if (klass != null) componentType = klass.javaObjectType
                             }
                         }
                     }
-                    when (type) {
-                        BOOLEAN_ARRAY,
-                        SHORT_ARRAY,
-                        INT_ARRAY,
-                        INT64_ARRAY,
-                        FLOAT_ARRAY,
-                        DOUBLE_ARRAY,
-                        STRING_ARRAY,
-                        JSONB_ARRAY -> {
-                            stmt.setArray(index, stmt.connection.createArrayOf(type.childType!!.text, arg))
+                    // Convert into component array.
+                    array = when (componentType) {
+                        Boolean::class.javaObjectType -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is Boolean) throw illegalArg("Illegal value in Boolean array at $it: '$v'")
+                            v
                         }
-                        BYTE_ARRAY_ARRAY -> {
-                            // This is a hack, because we need a `Byte[][]`, JDBC does not support an `Object[][]`,
-                            // even while the content may be the same, and it knows the type, still
-                            // Note: I guess the driver supports Object[][] for other types, because it can invoke
-                            //   helpers like `toString`, `toInt`, `toLong`, ... on them, but there is no such thing
-                            //   for byte-arrays (byte[]), and instead of writing an own toByteArray, they fail!
-                            val arr = Array(arg.size) { arg[it] as ByteArray? }
-                            stmt.setArray(index, stmt.connection.createArrayOf(type.childType!!.text, arr))
+                        Short::class.javaObjectType -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is Short) throw illegalArg("Illegal value in Short array at $it: '$v'")
+                            v
                         }
-                        BOOLEAN,
-                        SHORT,
-                        INT,
-                        INT64,
-                        FLOAT,
-                        DOUBLE,
-                        STRING,
-                        BYTE_ARRAY,
-                        JSONB -> throw illegalArg("The argument is $type, but an array was provided as value")
-                        null -> throw illegalArg("Failed to detect array type, no type-name was provided (null)")
-                        else -> throw illegalArg("Failed to detect array type, and invalid type-name was provided: $typeName")
+                        Int::class.javaObjectType -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is Int) throw illegalArg("Illegal value in Int array at $it: '$v'")
+                            v
+                        }
+                        Long::class.javaObjectType -> Array(array.size) {
+                            var v = arg[it]
+                            if (v is Int64) v = v.toLong()
+                            if (v != null && v !is Long) throw illegalArg("Illegal value in Long array at $it: '$v'")
+                            v
+                        }
+                        Float::class.javaObjectType -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is Float) throw illegalArg("Illegal value in Float array at $it: '$v'")
+                            v
+                        }
+                        Double::class.javaObjectType -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is Double) throw illegalArg("Illegal value in Double array at $it: '$v'")
+                            v
+                        }
+                        String::class.java -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is String) throw illegalArg("Illegal value in String array at $it: '$v'")
+                            v
+                        }
+                        ByteArray::class.java -> Array(array.size) {
+                            val v = arg[it]
+                            if (v != null && v !is ByteArray) throw illegalArg("Illegal value in ByteArray array at $it: '$v'")
+                            v
+                        }
+                        else -> throw illegalArg("Can't detect correct component type of empty arrays or arrays with only null values")
                     }
                 }
-                is ListProxy<*> -> setArgument(stmt, arg.toArray(), indices)
-                is MapProxy<*, *> -> stmt.setString(index, Platform.toJSON(arg))
-                else -> throw illegalArg("Unable to set argument: args[${index - 1}], unknown type: ${arg.javaClass.name}")
+                if (!validComponentJavaTypes.contains(componentType)) {
+                    throw illegalArg("Unsupported array type: ${componentType.name}")
+                }
+                for (index in indices) stmt.setObject(index, array)
             }
+            is MapProxy<*, *> -> {
+                val json = Platform.toJSON(arg)
+                for (index in indices) stmt.setString(index, json)
+            }
+            else -> throw illegalArg("Failed to bind argument to query, unsupported type: ${arg::class.qualifiedName}")
         }
     }
 
@@ -171,9 +208,10 @@ class PsqlQuery(query: String, private val typeNames: Array<String>?) {
         var i = 0
         while (i < args.size) {
             val arg = args[i]
+            val typeName = typeNames?.get(i)
             val indices = dollarToIndices[i + 1]
             check(indices != null) { "Indices must not be null - could not resolve index for \$${i + 1}" }
-            setArgument(stmt, arg, indices)
+            setArgument(stmt, arg, typeName, indices)
             i++
         }
     }
@@ -186,5 +224,15 @@ class PsqlQuery(query: String, private val typeNames: Array<String>?) {
         private const val SUSPICIOUSLY_MANY_ARGS = 1_000
         private val positiveNums = '1'..'9'
         private val nums = '0'..'9'
+        private val validComponentJavaTypes = setOf<Class<*>>(
+            Short::class.javaObjectType, Short::class.javaPrimitiveType!!,
+            Int::class.javaObjectType, Int::class.javaPrimitiveType!!,
+            Long::class.javaObjectType, Long::class.javaPrimitiveType!!,
+            Float::class.javaObjectType, Float::class.javaPrimitiveType!!,
+            Double::class.javaObjectType, Double::class.javaPrimitiveType!!,
+            Boolean::class.javaObjectType, Boolean::class.javaPrimitiveType!!,
+            String::class.java,
+            ByteArray::class.java
+        )
     }
 }
