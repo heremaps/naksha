359be6fe9df7ba860c01bae0bfcfe6b000274e04
Alexander Lowey-Weber <alexander.weber@here.com>
2026-09-04 12:47:27 +0200

    Fix that only sourceID_ has special treatment.
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagNormalizer.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagNormalizer.kt
@@ -10,31 +10,44 @@ import kotlin.js.JsExport
 import kotlin.jvm.JvmStatic
 
 /**
- * An object used for Tag normalization and splitting.
+ * An object used for Tag normalization and _(optional)_ splitting.
  *
  * Process of normalization happens in [normalizeTag] method and includes following steps:
- * 1) Always: apply normalization form (see [NormalizerForm])
- * 2) Conditional: lowercase the whole tag
+ *
+ * 1) Always: apply [UNICODE normalization form](https://www.unicode.org/reports/tr15/) (see [NormalizerForm])
+ * 2) Conditional: lowercase the tag
  * 3) Conditional: remove all non-ASCII characters
+ * 4) Optional: split
  *
- * Normalization form used in step #1 and subsequent conditional steps depend on tag prefix.
+ * Process of splitting tags is done when [TagList.toTagMap] is invoked. Note that not all tags can be split, it depends on their prefix.
  *
- * Process of splitting happens in [splitNormalizedTag] method.
- * Note that not all tags can be split, it depends on their prefix.
+ * Summarised per-prefix behavior of the tag-normalization:
  *
- * Summarised per-prefix behavior:
- * +----------+------------+-----------+----------+-------+
- * | prefix   | norm. form | lowercase | no ASCII | split |
- * +----------+------------+-----------+----------+-------+
- * | @        | NFKC       | false     | false    | true  |
- * | ref_     | NFKC       | false     | false    | false |
- * | ~        | NFD        | false     | true     | true  |
- * | #        | NFD        | false     | true     | true  |
- * | sourceID | NFKC       | false     | false    | false |
- * | < ELSE > | NFD        | true      | true     | true  |
- * +----------+------------+-----------+----------+-------+
+ * - `sourceID_.*` —> NFKC
+ *     - **lowercase**: `false`, **ASCII**: `false`, **split**: `false`
+ * - `ref_.*` —> NFKC, reference
+ *     - **lowercase**: `false`, **ASCII**: `false`, **split**: `false`
+ * - `@.*` —> NFKC, splittable reference
+ *     - **lowercase**: `false`, **ASCII**: `false`, **split**: `true`
+ * - `#.*` —> NFD,
+ *     - **lowercase**: `false`, **ASCII**: `true`, **split**: `true`
+ * - `~.*` —> NFD _(Web-URL friend variant of `#`)_
+ *     - **lowercase**: `false`, **ASCII**: `true`, **split**: `true`
+ * - otherwise: NFD
+ *     - **lowercase**: `true`, **ASCII**: `true`, **split**: `true`
  *
- * By default, (if no special prefix is found) tag is normalized with NFD, lowercased, cleaned of non-ASCII and splittable.
+ * Note, the `sourceID_.*` is a historic artifact, it was the first exception being made from normalization. In the aftermath of this, rules were introduced, specifically the `ref_.*` and `@.*` rule, to avoid more exceptions.
+ *
+ * By default, (if no special prefix is found) tag is normalized with NFD, lowercased, cleaned of non-ASCII and is splittable.
+ *
+ * If you you copy identifiers into tags, it is strongly recommended to use the `@` _(at)_ prefix. For example `@sourceId=Ref$1`. This keeps the tag unchanged and allows to query for that exact tag, plus it allows to split the tags in code to tests if there is a `sourceId`:
+ * ```kotlin
+ * val tagList = TagList("@sourceId=Ref$1")
+ * val tagMap = tagList.toTagMap()
+ * println( tagMap.containsKey("@sourceId") )
+ * println( tagMap.get("@sourceId") )
+ * ```
+ * @since 3.0
  */
 @JsExport
 class TagNormalizer private constructor() {
@@ -44,7 +57,7 @@ class TagNormalizer private constructor() {
         private val PREFIX_TO_POLICY = mapOf(
             "@" to TagProcessingPolicy(NFKC, removeNonAscii = false, lowercase = false, split = true),
             "ref_" to TagProcessingPolicy(NFKC, removeNonAscii = false, lowercase = false, split = false),
-            "sourceID" to TagProcessingPolicy(NFKC, removeNonAscii = false, lowercase = false, split = false),
+            "sourceID_" to TagProcessingPolicy(NFKC, removeNonAscii = false, lowercase = false, split = false),
             "~" to TagProcessingPolicy(NFD, removeNonAscii = true, lowercase = false, split = true),
             "#" to TagProcessingPolicy(NFD, removeNonAscii = true, lowercase = false, split = true)
         )
