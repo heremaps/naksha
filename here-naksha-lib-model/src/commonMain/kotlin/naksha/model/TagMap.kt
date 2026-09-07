8045988a84dbeb634886181ff1fd983403e21176
Alexander Lowey-Weber <alexander.weber@here.com>
2026-09-07 15:02:32 +0200

    Improve constructors of TagMap, update documentation.
    Signed-off-by: Alexander Lowey-Weber <alexander.weber@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagMap.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/TagMap.kt
@@ -2,33 +2,82 @@
 
 package naksha.model
 
-import naksha.base.ITagMap
 import naksha.base.MapProxy
 import naksha.base.NakshaError
 import naksha.base.NakshaException
+import naksha.base.illegalArg
+import naksha.model.TagNormalizer.TagNormalizer_C.joinTag
+import naksha.model.TagNormalizer.TagNormalizer_C.splitTag
 import kotlin.js.JsExport
 import kotlin.js.JsName
 
 /**
- * Map of tags persisted as (key, value) pairs where values are nullable.
- * This class represents the persisted form of [TagList].
- *
- * It is advised to only construct it in one of two ways:
- * 1) Via [TagList]-based constructor
- * 2) By deserializing bytes fetched from a storage
- *
- * If for some reason, one would like to use it otherwise, it is advised to properly prepare tags upfront
- * with use of [TagNormalizer] (that is used for example by [TagList])
+ * Map of tags with values as _(key, value)_ pairs. Beware that values are limited to the following primitives:
+ * - `null`
+ * - [Boolean]
+ * - [Double]
+ * - [String]
+ * @since 3.0
  */
 @JsExport
-class TagMap() : MapProxy<String, Any>(String::class, Any::class), ITagMap {
+class TagMap() : MapProxy<String, Any>(String::class, Any::class) {
 
-    @Suppress("LeakingThis")
-    @JsName("of")
+    /**
+     * Construct a new [TagMap] from the given list of key-value pairs.
+     * @param pairs a list of keys and values with the key being a [String] and the value being `null`, [Boolean], [Number] or [String].
+     * @since 3.0
+     * @throws NakshaException if the key is not a [String] or the value of any pair is not `null`, [Boolean], [Number] or [String].
+     */
+    @JsName("newTagMap")
+    constructor(vararg pairs: Any?) : this() {
+        var i = 0
+        while (i < pairs.size) {
+            val key: String = pairs[i] as? String? ?: throw illegalArg("The given key at $i is no String: ${pairs[i]}")
+            val value = if (++i < pairs.size) when(val v = pairs[i++]) {
+                null -> null
+                is Boolean -> v
+                is String -> v
+                is Double -> v
+                is Number -> v.toDouble()
+                else -> throw illegalArg("The given value is not boolean, a number or a string: $v")
+            } else null
+            i++
+            this[key] = value
+        }
+    }
+
+    /**
+     * Construct a new [TagMap] from the given list of key-value pairs.
+     * @param tags the key-value pairs.
+     * @since 3.0
+     * @throws NakshaException if the value of any pair is not `null`, [Boolean], [Number] or [String].
+     */
+    @JsName("ofPairs")
+    constructor(vararg tags: Pair<String, Any?>) : this() {
+        for ((k, v) in tags) {
+            this[k] = when (v) {
+                null -> null
+                is Boolean -> v
+                is String -> v
+                is Double -> v
+                is Number -> v.toDouble()
+                else -> throw illegalArg("The given value is not boolean, a number or a string: $v")
+            }
+        }
+    }
+
+    /**
+     * Construct a [TagMap] from the given [TagList].
+     * @param tagList the [TagList] to split and insert into this map.
+     * @since 3.0
+     */
+    @JsName("ofTagList")
     constructor(tagList: TagList) : this() {
-        tagList.filterNotNull()
-            .map { TagNormalizer.splitNormalizedTag(it) }
-            .forEach { (key, value) -> put(key, value) }
+        for (tag in tagList) {
+            if (tag == null) continue
+            val pair = splitTag(tag)
+            put(pair.first, pair.second)
+        }
     }
 
     /**
@@ -37,28 +86,7 @@ class TagMap() : MapProxy<String, Any>(String::class, Any::class), ITagMap {
      */
     fun toTagList(): TagList {
         val list = TagList()
-        forEach { (key, value) ->
-            list.add(flattenTag(key, value))
-        }
+        forEach { (key, value) -> list.add(joinTag(key, value)) }
         return list
     }
-
-    /**
-     * Converts (key, value) pair to String, so it can be part of [TagList].
-     * The result depends on the value:
-     * - Null value is omitted: ('foo', null) -> 'foo'
-     * - String value is separated with simple '=': ('foo', 'bar') -> 'foo=bar'
-     * - Numbers and booleans are separated with ':=' -> 'foo:=true', 'foo:=12.34'
-     */
-    private fun flattenTag(key: String, value: Any?): String =
-        when (value) {
-            null -> key
-            is String -> "$key=$value"
-            is Boolean, is Long, is Int64 -> "$key:=$value"
-            is Number -> "$key:=${value.toDouble()}"
-            else -> throw NakshaException(
-                NakshaError.ILLEGAL_ARGUMENT,
-                "Tag values can only be String, Boolean or Number"
-            )
-        }
 }
