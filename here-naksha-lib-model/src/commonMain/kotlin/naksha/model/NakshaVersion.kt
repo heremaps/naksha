0b251c1689395ab3a2491ba02f29a86a96a235fb
phmai <phuc.mai@here.com>
2026-09-08 16:41:28 +0200

    release beta.51
    Signed-off-by: phmai <phuc.mai@here.com>

--- here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
+++ here-naksha-lib-model/src/commonMain/kotlin/naksha/model/NakshaVersion.kt
@@ -88,7 +88,7 @@ class NakshaVersion(
          * The current version as string to constant usage cases.
          * @since 3.0
          */
-        const val CURRENT = "3.0.0-beta.50"
+        const val CURRENT = "3.0.0-beta.51"
         // WARNING: Do not update this property manually, it is automatically modified when building!
         //          Edit version only in `gradle.properties` file, which is used as well to create artifacts!
 
